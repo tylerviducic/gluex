1fa217a843ae78945296dbd95bb61faa7823977f
Tyler <tyler.viducic@gmail.com>
2023-04-27 11:36:10 -0400

    small string updates for mc_thrown running

--- f1_mc/analysis/RunF1Thrown_KsKmPip.C
+++ f1_mc/analysis/RunF1Thrown_KsKmPip.C
@@ -5,9 +5,9 @@ void RunF1Thrown_KsKmPip(){
     gROOT->ProcessLine("DPROOFLiteManager::Set_SandBox(\"/volatile/halld/home/viducic/PROOF/.proof\")");
     gROOT->ProcessLine("TChain* ch = new TChain(\"Thrown_Tree\")"); 
     // TChain* ch = new TChain("Thrown_Tree");
-    gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/f1_pipkmks_s18_100m_v16_rt_20220419034710pm/root/thrown/*.root\")"); //spring
+    // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/f1_pipkmks_s18_100m_v16_rt_20220419034710pm/root/thrown/*.root\")"); //spring
     // ch->Add("/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/f1_pipkmks_s18_100m_v16_20220409110456am/root/thrown/*.root"); //spring
-    // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/f1_pipkmks_f18_100m_v16_rt_20220419034345pm/root/thrown/*.root\")"); //fall
+    gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/f1_pipkmks_f18_100m_v16_rt_20220419034345pm/root/thrown/*.root\")"); //fall
     // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/pipkmks_20220310040819pm/root/thrown/*.root\")"); //2017
     gROOT->ProcessLine("DPROOFLiteManager::Process_Chain(ch, \"DSelector_mc_pipkmks_thrown.C++\", 4)");
 
