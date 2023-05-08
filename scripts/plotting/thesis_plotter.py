04c751d3e7a24f5f5f59b08be6ec4d7319cbe2d3
Tyler <tyler.viducic@gmail.com>
2023-05-08 09:11:56 -0400

    thesis plotting

--- scripts/plotting/thesis_plotter.py
+++ scripts/plotting/thesis_plotter.py
@@ -1,6 +1,12 @@
 # barebones script for plotting things for things for thesis 
 
 import ROOT
-    # import numpy as np
-    # import pandas as pd
-    # import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+data_file = '/work/halld/home/viducic/data/pipkmks/data/bestX2/pipkmks_flat_bestX2_2017.root'
+file_treename = 'pipkmks__B4_M16'
+
+df = ROOT.RDataFrame(file_treename, data_file)
+print("maybe i should do this in a notebook...")
