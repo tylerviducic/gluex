d06b7fdf34eeadc879f974ae8c26726cee1de468
Tyler <tyler.viducic@gmail.com>
2023-05-09 15:30:27 -0400

    double gaussian fit works well

--- scripts/fitting/roofit/kshort_width.py
+++ scripts/fitting/roofit/kshort_width.py
@@ -15,27 +15,27 @@ df = df.Define('ks_m', "sqrt(ks_E*ks_E - ks_px*ks_px - ks_py*ks_py - ks_pz*ks_pz
 
 ks_pathlength_cut = 'pathlength_sig > 5'
 
-data_hist = df.Filter(ks_pathlength_cut).Histo1D(('ks_m', 'ks_m', 1000, 0.35, 0.65), 'ks_m').GetValue()
+data_hist = df.Filter(ks_pathlength_cut).Histo1D(('ks_m', 'ks_m', 500, 0.35, 0.65), 'ks_m').GetValue()
 
 
 m_pipi = ROOT.RooRealVar("m_pipi", "m_pipi", 0.35, 0.65)
 dh = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(m_pipi), data_hist)
 
-m_pipi.setRange("signal", 0.45, 0.55)
+m_pipi.setRange("signal", 0.4, 0.6)
 
 ks_mean_1 = ROOT.RooRealVar("mean_1", "mean_1", 0.5, 0.475, 0.515)
-ks_sigma_1 = ROOT.RooRealVar("sigma_1", "sigma_1", 0.01, 0.004, 0.02)
+ks_sigma_1 = ROOT.RooRealVar("sigma_1", "sigma_1", 0.01, 0.002, 0.02)
 
 # ks_mean.setConstant(1)
 # ks_sigma.setConstant(1)
 gaus_1 = ROOT.RooGaussian("gaus_1", "gaus_1", m_pipi, ks_mean_1, ks_sigma_1)
 
 ks_mean_2 = ROOT.RooRealVar("mean_2", "mean_2", 0.5, 0.475, 0.515)
-ks_sigma_2 = ROOT.RooRealVar("sigma_2", "sigma_2", 0.01, 0.004, 0.02)
+ks_sigma_2 = ROOT.RooRealVar("sigma_2", "sigma_2", 0.01, 0.002, 0.02)
 
-gaus_2 = ROOT.RooGaussian("gaus_2", "gaus_2", m_pipi, ks_mean_1, ks_sigma_1)
+gaus_2 = ROOT.RooGaussian("gaus_2", "gaus_2", m_pipi, ks_mean_2, ks_sigma_2)
 
-gaus_frac = ROOT.RooRealVar("gaus_frac", "gaus_frac", 0.05, 0.0, 1.0)
+gaus_frac = ROOT.RooRealVar("gaus_frac", "gaus_frac", 0.95, 0.0, 1.0)
 double_gaus = ROOT.RooAddPdf("double_gaus", "double_gaus", ROOT.RooArgList(gaus_1, gaus_2), ROOT.RooArgList(gaus_frac))
 
 
@@ -49,15 +49,18 @@ bkg = ROOT.RooPolynomial("bkg", "bkg", m_pipi, ROOT.RooArgList(bkg_par1))
 
 sig_frac = ROOT.RooRealVar("sig_frac", "sig_frac", 0.5, 0.0, 1.0)
 
-composite = ROOT.RooAddPdf("composite", "composite", ROOT.RooArgList(gaus, bkg), ROOT.RooArgList(sig_frac))
+composite = ROOT.RooAddPdf("composite", "composite", ROOT.RooArgList(double_gaus, bkg), ROOT.RooArgList(sig_frac))
 # composite.chi2FitTo(dh)
 composite.fitTo(dh)
+# composite.fitTo(dh,ROOT.RooFit.SumCoefRange("signal"))
 
 frame = m_pipi.frame()
 dh.plotOn(frame)
 composite.plotOn(frame)
 composite.plotOn(frame, ROOT.RooFit.Components("bkg"), ROOT.RooFit.LineStyle(ROOT.kDashed))
-composite.plotOn(frame, ROOT.RooFit.Components("gaus"), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.LineColor(ROOT.kRed))
+composite.plotOn(frame, ROOT.RooFit.Components("double_gaus"), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.LineColor(ROOT.kRed))
+composite.plotOn(frame, ROOT.RooFit.Components("gaus_1"), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.LineColor(ROOT.kGreen))
+composite.plotOn(frame, ROOT.RooFit.Components("gaus_2"), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.LineColor(ROOT.kBlue))
 
 frame.Draw()
 
