5ece1e3bae768d5eefc0126f6afb42e2f66427e5
Tyler <tyler.viducic@gmail.com>
2023-05-09 18:08:06 -0400

    testing import of common tools. it works!

---
+++ scripts/fitting/roofit/f1_resolution.py
@@ -0,0 +1,8 @@
+# script to get resolution of f1 to evaluate effectiveness of potential Ks constraint
+
+import ROOT
+import common_analysis_tools as tools
+
+run_dict = tools.run_dict
+
+#TODO apply all cuts to MC annd fit f1 signal to get width. it (should???) will be a breit-wigner
