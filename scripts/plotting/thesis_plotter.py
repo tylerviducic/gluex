726013ac581d360fb81b90af50dcdcddf7d23a40
Tyler <tyler.viducic@gmail.com>
2023-05-09 22:03:41 -0400

    return file path and treename in tuple

--- scripts/plotting/thesis_plotter.py
+++ scripts/plotting/thesis_plotter.py
@@ -8,73 +8,79 @@ from common_analysis_tools import *
 # import pandas as pd
 # import matplotlib.pyplot as plt
 
-#TODO add treename to these functions, return list or tuple
-
-def get_flat_data_file(channel, run_period, comboloop=False, filtered=True, hist=False):
+def get_flat_data_file_and_tree(channel, run_period, comboloop=False, filtered=True, hist=False):
     file_path = f'/work/hall/home/viducic/data/{channel}/data'
     treename = ''
     if not comboloop:
         file_path += f'/bestX2/{channel}_'
         if filtered:
             file_path += f'flat_bestX2_{run_dict[run_period]}.root'
+            treename = f'{channel}_filetered_{run_dict[run_period]}'
         else:
             if hist:
                 file_path += f'flat_result_{run_dict[run_period]}.root'
             else:
                 file_path += f'flat_bestX2_{run_dict[run_period]}.root'
+                treename = f'{channel}__B4_M16'
     else:
         file_path += f'/comboloop/{channel}_comboloop_flat_{run_dict[run_period]}.root'
-    return file_path
+        treename = f'{channel}__B4_M16'
+    return (file_path, treename)
 
-def get_flat_signal_file(channel, run_period, comboloop=False, filtered=True, hist=False):
+def get_flat_signal_file_and_tree(channel, run_period, comboloop=False, filtered=True, hist=False):
     file_path = f'/work/hall/home/viducic/data/{channel}/mc/signal/mc_{channel}_'
+    treename = ''
     if not comboloop:
         if filtered:
             file_path += f'filtered_{run_dict[run_period]}.root'
+            treename = f'mc_{channel}_filtered_{run_dict[run_period]}'
         else:
             if hist:
                 file_path += f'result_{run_dict[run_period]}.root'
             else:
                 file_path += f'flat_bestX2{run_dict[run_period]}.root'
+                treename = f'{channel}__ks_pippim__B4_M16'
     else:
         print('no comboloop signal mc file yet')
-    return file_path
+    return (file_path, treename)
 
-def get_flat_phasespace_file(channel, run_period, comboloop=False, filtered=True, hist=False):
+def get_flat_phasespace_file_and_tree(channel, run_period, comboloop=False, filtered=True, hist=False):
     file_path = f'/work/hall/home/viducic/data/{channel}/mc/phasespace/mc_{channel}_phasespace_'
     if not comboloop:
         if filtered:
             file_path += f'filtered_{run_dict[run_period]}.root'
+            treename = f'mc_{channel}_phasespace_filtered_{run_dict[run_period]}'
         else:
             if hist:
                 file_path += f'result_{run_dict[run_period]}.root'
             else:
                 file_path += f'flat_bestX2{run_dict[run_period]}.root'
+                treename = f'{channel}__ks_pippim__B4_M16'
     else:
         print('no comboloop phasespace mc file yet')
-    return file_path
+    return (file_path, treename)
 
-def get_flat_thrown_file(channel, run_period):
+def get_flat_thrown_file_and_tree(channel, run_period):
     return f'/work/hall/home/viducic/data/{channel}/mc/thrown/mc_{channel}_thrown_flat_result_{run_dict[run_period]}.root'
 
 
-def get_needed_flat_file(channel, run_period, datatype, comboloop=False, filtered=True, hist=False):
+def get_needed_flat_file_and_tree(channel, run_period, datatype, comboloop=False, filtered=True, hist=False):
+    file_tuple = ()
     if datatype == 'data':
-        filepath = get_flat_data_file(channel, run_period, comboloop, filtered, hist)
-        print(f'filepath: {filepath}')
+        file_tuple = get_flat_data_file_and_tree(channel, run_period, comboloop, filtered, hist)
     elif datatype == 'signal':
-        filepath = get_flat_signal_file(channel, run_period, comboloop, filtered, hist)
+        file_tuple = get_flat_signal_file_and_tree(channel, run_period, comboloop, filtered, hist)
     elif datatype == 'phasespace':
-        filepath = get_flat_phasespace_file(channel, run_period, comboloop, filtered, hist)
+        file_tuple = get_flat_phasespace_file_and_tree(channel, run_period, comboloop, filtered, hist)
     elif datatype == 'thrown':
-        filepath = get_flat_thrown_file(channel, run_period)
+        file_tuple = get_flat_thrown_file_and_tree(channel, run_period)
     else:
         print('invalid datatype')
         return
-    return filepath
+    print(f'filepath: {file_tuple[0]}')
+    return file_tuple
 
-
-
+#TODO write this function
 def plot_data_kshort_no_cuts():
     data_file = '/work/halld/home/viducic/data/pipkmks/data/bestX2/pipkmks_flat_bestX2_2017.root'
     file_treename = 'pipkmks__B4_M16'
