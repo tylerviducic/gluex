3ade2a5376d8014290e4af3bb9afbf99279c9017
Tyler <tyler.viducic@gmail.com>
2023-05-16 08:56:50 -0400

    fit w/ background hist (it sucks)

--- scripts/fitting/roofit/fit_single_kstar_cut.py
+++ scripts/fitting/roofit/fit_single_kstar_cut.py
@@ -1,39 +1,50 @@
 # script for fitting ksk-pi+ distribution with a cut applied to one of the other K*
 
 import ROOT
+from common_analysis_tools import *
 
 #TODO fit acceptance corrected distributions because background is not smooth
 
 channel = 'pipkmks'
-run_period = 'fall'
-selector_method = 'bestX2'
-run_dict = {
-    'fall': '2018_fall',
-    'spring': '2018_spring',
-    '2017': '2017'
-}
+run_period = 'spring'
+datatype = 'data'
+
 
 beam = 9
 kstar_cut = 'plus'
-bin_width = 'narrow'
 t_bin = 3
 beam_range = f'{beam - 0.5}_{beam+0.5}'
 
-t_bin_dict = {1: '0.1-0.2', 2: '0.2-0.3', 3: '0.3-0.4',
-              4: '0.4-0.65', 5: '0.65-0.9', 
-              6: '0.9-1.4', 7: '1.4-1.9'}
+data_file_and_tree = get_flat_file_and_tree(channel, run_period, datatype, filtered=False, hist=True)
+data_file = ROOT.TFile.Open(data_file_and_tree[0], 'READ')
+
+recon_phasespace_file_and_tree = get_flat_file_and_tree(channel, run_period, 'phasespace', filtered=False, hist=True)
+thrown_phasespace_file_and_tree = get_flat_thrown_file_and_tree(channel, run_period, phasespace=True)
+
+
+recon_file = ROOT.TFile.Open(recon_phasespace_file_and_tree[0], 'READ')
+thrown_file = ROOT.TFile.Open(thrown_phasespace_file_and_tree[0], 'READ')
 
 # filename = f'/work/halld/home/viducic/selector_output/f1_flat/{channel}_flat_result_{run_dict[run_period]}.root'
-filename = f'/work/halld/home/viducic/data/{channel}/data/{selector_method}/{channel}_flat_result_{run_dict[run_period]}.root'
-data_file = ROOT.TFile.Open(filename, 'READ')
-data_hist_name = channel + '_beam_' + beam_range+ '_cut_kstar_' + kstar_cut + f'_cut_t_{t_bin_dict[t_bin]}_' + bin_width +';1'
+# filename = f'/work/halld/home/viducic/data/{channel}/data/{selector_method}/{channel}_flat_result_{run_dict[run_period]}.root'
+# data_file = ROOT.TFile.Open(filename, 'READ')
+data_hist_name = channel + '_cut_kstar_' + kstar_cut + '_cut_beam_' + beam_range+ f'_t_{t_bin_dict[t_bin]}' +';1'
 data_hist = data_file.Get(data_hist_name)
+recon_hist = recon_file.Get(data_hist_name)
+
+thrown_hist_name = channel + '_beam_' + beam_range + f'_t_{t_bin_dict[t_bin]}' +';1'
+thrown_hist = thrown_file.Get(thrown_hist_name)
+
+acceptance_hist = recon_hist.Clone()
+acceptance_hist.Divide(thrown_hist)
 
 # data_hist.Draw()
 # input('Press enter to continue...')
 
-x = ROOT.RooRealVar("x", "x", 1.125, 1.7)
+x = ROOT.RooRealVar("x", "x", 1.2, 1.8)
 dh = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(x), data_hist)
+recon_ps_dh = ROOT.RooDataHist("recon_ps_dh", "recon_ps_dh", ROOT.RooArgList(x), recon_hist)
+
 
 # x.setRange("signal", 1.15, 2.0)
 
@@ -51,14 +62,19 @@ bw = ROOT.RooBreitWigner("bw", "bw", x, bw_m, bw_width)
 
 ## BACKGROUND OPTIONS ##
 
+## HIST ##
+
+bkg = ROOT.RooHistPdf("bkg_pdf", "bkg_pdf", ROOT.RooArgList(x), recon_ps_dh, 0)
+
+
 ## CHEBYCHEV ##
 
-bkg_par1 = ROOT.RooRealVar("bkg_par1", "bkg_par1", -1.0, 1.0)
-bkg_par2 = ROOT.RooRealVar("bkg_par2", "bkg_par2", -1.0, 1.0)
-bkg_par3 = ROOT.RooRealVar("bkg_par3", "bkg_par3", -1.0, 1.0)
-bkg_par4 = ROOT.RooRealVar("bkg_par4", "bkg_par4", -1.0, 1.0)
+# bkg_par1 = ROOT.RooRealVar("bkg_par1", "bkg_par1", -1.0, 1.0)
+# bkg_par2 = ROOT.RooRealVar("bkg_par2", "bkg_par2", -1.0, 1.0)
+# bkg_par3 = ROOT.RooRealVar("bkg_par3", "bkg_par3", -1.0, 1.0)
+# bkg_par4 = ROOT.RooRealVar("bkg_par4", "bkg_par4", -1.0, 1.0)
 
-bkg = ROOT.RooChebychev("bkg", "bkg", x, ROOT.RooArgList(bkg_par1, bkg_par2, bkg_par3))
+# bkg = ROOT.RooChebychev("bkg", "bkg", x, ROOT.RooArgList(bkg_par1, bkg_par2, bkg_par3))
 
 ## BERSTEIN ##
 
@@ -91,9 +107,9 @@ combined_pdf.fitTo(dh)
 frame = x.frame()
 dh.plotOn(frame)
 combined_pdf.plotOn(frame, ROOT.RooFit.LineColor(ROOT.kRed))
-combined_pdf.plotOn(frame, ROOT.RooFit.Components("bkg"), ROOT.RooFit.LineColor(ROOT.kViolet))
-combined_pdf.plotOn(frame, ROOT.RooFit.Components("bw"), ROOT.RooFit.LineColor(ROOT.kGreen))
-combined_pdf.plotOn(frame, ROOT.RooFit.Components("relbw"), ROOT.RooFit.LineColor(ROOT.kBlue))
+# combined_pdf.plotOn(frame, ROOT.RooFit.Components("bkg"), ROOT.RooFit.LineColor(ROOT.kViolet))
+# combined_pdf.plotOn(frame, ROOT.RooFit.Components("bw"), ROOT.RooFit.LineColor(ROOT.kGreen))
+# combined_pdf.plotOn(frame, ROOT.RooFit.Components("relbw"), ROOT.RooFit.LineColor(ROOT.kBlue))
 
 frame.Draw()
 
