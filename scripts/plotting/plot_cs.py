94163c3cb01959c69ca599bacc515389c38ee0b5
Tyler <tyler.viducic@gmail.com>
2023-06-09 12:27:02 -0400

    iterate over beam energies with "group_by"

--- scripts/plotting/plot_cs.py
+++ scripts/plotting/plot_cs.py
@@ -9,36 +9,38 @@ channel = 'pipkmks'
 filename = f'/work/halld/home/viducic/data/fit_params/{channel}/cross_section_values.csv'
 
 df = pandas.read_csv(filename)
-print(df.to_string())
+# print(df.to_string())
 
 variables = ['mean', 'width', 'yield', 'cross_section', 'chi2ndf']
 
-beam_enery = 10
-df = df.loc[df['beam_energy']==beam_enery]
 
-for variable in variables:
-    # Select the data for the current plot
+grouped = df.groupby('beam_energy')
 
-    x = df['t_bin_middle'].values
-    y = df[variable].values
-    x_err = df['t_bin_width'].values
+for name, group in grouped:
+    # print(f'name = {name}')
+    for variable in variables:
+        # Select the data for the current plot
+
+        x = group['t_bin_middle'].values
+        y = group[variable].values
+        x_err = group['t_bin_width'].values
 
 
-    if(variable == 'chi2ndf'):
-        y_err = numpy.zeros(len(x))
-    else:
-        y_err = df[variable+'_error'].values
+        if(variable == 'chi2ndf'):
+            y_err = numpy.zeros(len(x))
+        else:
+            y_err = group[variable+'_error'].values
 
-    # Create a new figure and axis for the scatterplot
-    fig, ax = plt.subplots()
+        # Create a new figure and axis for the scatterplot
+        fig, ax = plt.subplots()
 
-    # Plot the data with error bars
-    ax.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='o')
+        # Plot the data with error bars
+        ax.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='o')
 
-    # Set the axis labels
-    ax.set_xlabel(latex_axis_label_dict['t'])
-    ax.set_ylabel(latex_axis_label_dict[variable])
+        # Set the axis labels
+        ax.set_xlabel(latex_axis_label_dict['t'])
+        ax.set_ylabel(latex_axis_label_dict[variable])
 
-    # Show or save the plot
-    # plt.show()  # or 
-    plt.savefig(f'/work/halld/home/viducic/plots/cross_section/{channel}_plot_of_{variable}_beam-{beam_enery}.png')
+        # Show or save the plot
+        # plt.show()  # or 
+        plt.savefig(f'/work/halld/home/viducic/plots/cross_section/{channel}_plot_of_{variable}_beam-{name}.png')
