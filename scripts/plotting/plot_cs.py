3a247e2e55852c44754f5bc315c98a1b6069f1d8
Tyler <tyler.viducic@gmail.com>
2023-06-09 20:09:16 -0400

    plotting vs theory

--- scripts/plotting/plot_cs.py
+++ scripts/plotting/plot_cs.py
@@ -4,9 +4,11 @@ import pandas
 import numpy
 import matplotlib.pyplot as plt
 from common_analysis_tools import *
+import pandas as pd
 
 channel = 'pipkmks'
 filename = f'/work/halld/home/viducic/data/fit_params/{channel}/cross_section_values.csv'
+theory_filename = '/work/halld/home/viducic/theory_predictions/t-slope-{}GeVnew.dat'
 
 df = pandas.read_csv(filename)
 # print(df.to_string())
@@ -36,6 +38,12 @@ for name, group in grouped:
 
         # Plot the data with error bars
         ax.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='o')
+        if(variable == 'cross_section'):
+            theory_df = pd.read_csv(theory_filename.format(name), delim_whitespace=True)
+            theory_df.columns = ['t', 'diff_cs']
+            theory_df = theory_df.loc[(theory_df['t'] > 0.1) & (theory_df['t'] < 1.9)]
+            theory_df['diff_cs'] = theory_df['diff_cs']
+            ax.plot(theory_df['t'], theory_df['diff_cs'], 'r-')
 
         # Set the axis labels
         ax.set_xlabel(latex_axis_label_dict['t'])
