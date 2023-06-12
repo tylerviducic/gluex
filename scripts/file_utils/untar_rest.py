3587b8762544d51058bba4239c73c5c7e1c91345
Tyler <tyler.viducic@gmail.com>
2023-06-12 18:53:10 -0400

    script to untar rest files

---
+++ scripts/file_utils/untar_rest.py
@@ -0,0 +1,21 @@
+# file to untar rest files on /volatile
+
+import os
+import tarfile
+
+# get list of files in the directory
+topdir = '/lustre19/expphy/volatile/halld/home/viducic/REST/pimkpks_signal_2017'
+files = os.listdir(topdir)
+
+# loop over files
+for f in files:
+    # check if file is a tar file
+    if f.endswith('.tar'):
+    # Create the full file path
+        file_path = os.path.join(topdir, f)
+
+        # Extract the .tar file
+        with tarfile.open(file_path, 'r') as tar:
+            tar.extractall(topdir)
+
+        print(f"Extracted {f} successfully.")
