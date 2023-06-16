ea70e464146eefbd093ecea5d0b816b8632c1871
Tyler <tyler.viducic@gmail.com>
2023-06-16 15:02:53 -0400

    added mxp no kinfit together

--- skimming/combine_mxp_rf_output.py
+++ skimming/combine_mxp_rf_output.py
@@ -7,12 +7,12 @@ import glob
 print("Combining files for mxp reaction_filter..")
 
 subdirs = []
-topdir = '/volatile/halld/home/viducic/pipkmks_mpx/tree_pipkmksmissprot__ks_pippim__B4_M16/'
+topdir = '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/mxp_ks_constrained/tree_pipkmksmissprot__ks_pippim__B4'
 for root, dirs, files in os.walk(topdir):
     for subdir in dirs:
         subdirs.append(subdir)
 
-command = 'hadd /lustre19/expphy/volatile/halld/home/viducic/pipkmks_mpx/combined_runs/tree_pipkmksmissprot__ks_pippim__B4_M16_{}.root /lustre19/expphy/volatile/halld/home/viducic/pipkmks_mpx/tree_pipkmksmissprot__ks_pippim__B4_M16/{}/*'
+command = 'hadd /lustre19/expphy/volatile/halld/home/viducic/mxp_study/mxp_ks_constrained/combined_runs/tree_pipkmksmissprot__ks_pippim__B4_{}.root /lustre19/expphy/volatile/halld/home/viducic/mxp_study/mxp_ks_constrained/tree_pipkmksmissprot__ks_pippim__B4/{}/*'
 
 for subdir in subdirs:
     # print(command.format(subdir, subdir))
