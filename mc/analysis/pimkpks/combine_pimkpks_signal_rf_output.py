c585172644db94e189df6623298bc17d5d507df1
Tyler <tyler.viducic@gmail.com>
2023-06-20 15:17:27 -0400

    add files and write to /volatile

--- mc/analysis/pimkpks/combine_pimkpks_signal_rf_output.py
+++ mc/analysis/pimkpks/combine_pimkpks_signal_rf_output.py
@@ -4,15 +4,15 @@ import os
 import subprocess
 import glob
 
-print("Combining files for mxp reaction_filter..")
+print("Combining files for pimkpks mc reaction_filter..")
 
 subdirs = []
-topdir = '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_pd/tree_pipkmks__ks_pippim__B4_M16'
+topdir = '/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/tree_pimkpks__ks_pippim__B4_M16'
 for root, dirs, files in os.walk(topdir):
     for subdir in dirs:
         subdirs.append(subdir)
 
-command = 'hadd /lustre19/expphy/volatile/halld/home/viducic/pipkmks_pd/combined_runs/tree_pipkmks__ks_pippim__B4_M16_{}.root {}/{}/*'
+command = 'hadd /lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_{}.root {}/{}/*'
 
 for subdir in subdirs:
     # print(command.format(subdir, subdir))
