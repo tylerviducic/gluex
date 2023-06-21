c88f8d7f4ec7cfa55e6c82d830c2aae02044b966
Tyler <tyler.viducic@gmail.com>
2023-06-21 16:24:39 -0400

    loop over all potential dirs

--- scripts/file_utils/keep_mxp_data.py
+++ scripts/file_utils/keep_mxp_data.py
@@ -4,12 +4,19 @@ import ROOT
 import os
 
 # get list of files in directory 
-topdir = '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_mpx/combined_runs'
-files = os.listdir(topdir)
+topdirs = [
+    '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_mpx/combined_runs'
+    '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/pd_ks_constrained/combined_runs'
+    '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/mxp_ks_constrained/combined_runs'
+    '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_pd/combined_runs'
+]
 
-# open and close each file without performing any other operations
-for file in files:
-    f = ROOT.TFile.Open(topdir + '/' + file)
-    f.Close()
+for topdir in topdirs:
+    files = os.listdir(topdir)
+
+    # open and close each file without performing any other operations
+    for file in files:
+        f = ROOT.TFile.Open(topdir + '/' + file)
+        f.Close()
 
 print('Done!')
