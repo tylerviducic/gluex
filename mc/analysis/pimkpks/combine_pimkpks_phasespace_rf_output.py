2193a463288998cdb1184801a9049b42f506c551
Tyler <tyler.viducic@gmail.com>
2023-06-26 10:47:37 -0400

    add together phasespace jobs

---
+++ mc/analysis/pimkpks/combine_pimkpks_phasespace_rf_output.py
@@ -0,0 +1,19 @@
+# combine files from bggen
+
+import os
+import subprocess
+import glob
+
+print("Combining files for pimkpks mc phasespace reaction_filter..")
+
+subdirs = []
+topdir = '/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/phasespace/tree_pimkpks__ks_pippim__B4_M16'
+for root, dirs, files in os.walk(topdir):
+    for subdir in dirs:
+        subdirs.append(subdir)
+
+command = 'hadd /lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/phasespace/combined_runs/tree_pimkpks__ks_pippim__B4_M16_{}.root {}/{}/*'
+
+for subdir in subdirs:
+    # print(command.format(subdir, subdir))
+    subprocess.run(command.format(subdir, topdir, subdir), shell=True)
