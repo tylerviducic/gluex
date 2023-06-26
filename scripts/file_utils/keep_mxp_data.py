06e920a439a5334aa26b8f82d64a9965f61a8a6d
Tyler <tyler.viducic@gmail.com>
2023-06-26 18:16:50 -0400

    keep files updated

--- scripts/file_utils/keep_mxp_data.py
+++ scripts/file_utils/keep_mxp_data.py
@@ -5,18 +5,26 @@ import os
 
 # get list of files in directory 
 topdirs = [
-    '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_mpx/combined_runs'
-    '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/pd_ks_constrained/combined_runs'
-    '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/mxp_ks_constrained/combined_runs'
+    '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_mpx/combined_runs',
+    '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/pd_ks_constrained/combined_runs',
+    '/lustre19/expphy/volatile/halld/home/viducic/mxp_study/mxp_ks_constrained/combined_runs',
     '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_pd/combined_runs'
 ]
 
-for topdir in topdirs:
-    files = os.listdir(topdir)
+files =[]
 
-    # open and close each file without performing any other operations
-    for file in files:
-        f = ROOT.TFile.Open(topdir + '/' + file)
-        f.Close()
+#loop over all the files in the top directory and add them to a list
+for topdir in topdirs:
+    for file in os.listdir(topdir):
+        print(file)
+        if file.endswith('.root'):
+            files.append(topdir + "/" + file)
+
+
+# open and close each file without performing any other operations
+for file in files:
+    print('Opening file: ' + file)
+    f = ROOT.TFile.Open(file)
+    f.Close()
 
 print('Done!')
