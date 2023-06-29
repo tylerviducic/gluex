2b7eb5bef2be5a23ba58ffaeea42fa799d025b39
Tyler <tyler.viducic@gmail.com>
2023-06-29 17:44:23 -0400

    added 2D histo

--- scripts/plotting/beam_photon_thrown_vs_recon.py
+++ scripts/plotting/beam_photon_thrown_vs_recon.py
@@ -13,6 +13,8 @@ thrown_treename = 'pipkmks_thrown'
 recon_df = ROOT.RDataFrame(recon_file_and_tree[1], recon_file_and_tree[0])
 thrown_df = ROOT.RDataFrame(thrown_treename, thrown_filename)
 
+thrown_df = thrown_df.Define("P_p", "sqrt(Proton_px*Proton_px + Proton_py*Proton_py + Proton_pz*Proton_pz)")
+
 hist_beam_thrown = thrown_df.Histo1D(('hist_beam_thrown', 'hist_beam_thrown', 200, 5, 11.5), 'Beam_E').GetValue()
 hist_beam_recon = recon_df.Histo1D(('hist_beam_recon', 'hist_beam_recon', 200, 5, 11.5), 'e_beam')
 
@@ -25,13 +27,21 @@ hist_beam_recon.Scale(1/hist_beam_recon.Integral())
 hist_beam_recon.SetLineColor(ROOT.TColor.GetColor(colorblind_hex_dict['blue']))
 hist_beam_thrown.SetLineColor(ROOT.TColor.GetColor(colorblind_hex_dict['red']))
 
+hist_pp_thrown_vs_beam = thrown_df.Histo2D(('hist_pp_thrown_vs_beam', 'hist_pp_thrown_vs_beam', 200, 5, 11.5, 100, 0, 1.0), 'Beam_E', 'P_p')
+
+
 c = ROOT.TCanvas('c', 'c', 1200, 900)
 hist_beam_thrown.Draw("hist")
 hist_beam_recon.Draw('same hist')
 c.Update()
 
 c1 = ROOT.TCanvas('c1', 'c1', 1200, 900)
+c1.Divide(2, 1)
+c1.cd(1)
 acceptance.Draw('hist')
+c1.cd(2)
+hist_pp_thrown_vs_beam.Draw('colz')
 c1.Update()
+
 
 input('Press enter to exit')
