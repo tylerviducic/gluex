358430aa172a6009f95a8a7acd5d636c154e88cd
Tyler <tyler.viducic@gmail.com>
2023-07-07 15:00:11 -0400

    working on flux corrected yields

--- scripts/crosssection/flux_corrected_yields.py
+++ scripts/crosssection/flux_corrected_yields.py
@@ -4,6 +4,53 @@ import ROOT
 import pandas as pd
 import matplotlib.pyplot as plt
 from common_analysis_tools import *
+import seaborn as sns
 
-channel = 'pipkmks'
-binned_fit_results = f'/work/halld/home/viducic/data/fit_params/{channel}/cross_section_values.csv'
+sns.set_theme()
+
+def combine_both_channels():
+    df_pipkmks = pd.read_csv('/work/halld/home/viducic/data/fit_params/pipkmks/cross_section_values.csv')
+    df_pipkmks['channel'] = 'pipkmks'
+    df_pipkmks_acceptance = pd.read_csv('/work/halld/home/viducic/data/fit_params/pipkmks/binned_integrated_acceptance.csv')
+    print(df_pipkmks_acceptance.to_string())
+    df_pipkmks = df_pipkmks.merge(df_pipkmks_acceptance, on=['beam_energy', 't_bin_middle'], how='left')
+    df_pimkpks = pd.read_csv('/work/halld/home/viducic/data/fit_params/pimkpks/cross_section_values.csv')
+    df_pimkpks['channel'] = 'pimkpks'
+    df_pimkpks_acceptance = pd.read_csv('/work/halld/home/viducic/data/fit_params/pimkpks/binned_integrated_acceptance.csv')
+    df_pimkpks = df_pimkpks.merge(df_pimkpks_acceptance, on=['beam_energy', 't_bin_middle'], how='left')
+    df = pd.concat([df_pipkmks, df_pimkpks], ignore_index=True)
+    return df
+
+def single_channel(channel):
+    binned_fit_results = f'/work/halld/home/viducic/data/fit_params/{channel}/cross_section_values.csv'
+    integrated_acceptance_results = f'/work/halld/home/viducic/data/fit_params/{channel}/binned_integrated_acceptance.csv'
+
+    df_fit = pd.read_csv(binned_fit_results)
+    df_acc = pd.read_csv(integrated_acceptance_results)
+    df = df_fit.merge(df_acc, on=['beam_energy', 't_bin_middle'], how='left')
+    df['channel'] = channel
+    return df
+
+
+if __name__ == "__main__":
+    # channel = 'pipkmks'
+    # channel = 'pimkpks'
+    channel = 'combined'
+    if channel == 'combined':
+        df = combine_both_channels()
+    elif channel == 'pipkmks' or channel == 'pimkpks':
+        df = single_channel(channel)
+    else:
+        "Invalid channel. Exiting."
+        exit(1)
+
+    df['beam_lower'] = df['beam_energy'] - 0.5
+    df['beam_higher'] = df['beam_energy'] + 0.5
+    df['lumi'] = df.apply(lambda row: get_luminosity_gluex_1(row['beam_lower'], row['beam_higher']), axis=1)
+
+    df['flux_corrected_yield'] = df['yield'] * df['acceptance']/(df['lumi'] * df['t_bin_width'])
+
+    sns.relplot(data=df, x='t_bin_middle', y='flux_corrected_yield', hue='beam_energy', style='channel', palette='viridis')
+    plt.savefig(f'/work/halld/home/viducic/plots/cross_section/{channel}_flux_corrected_yield.png')
+
+
---
+++ scripts/crosssection/get_binned_integrated_acceptance copy.py
@@ -0,0 +1,41 @@
+# script to get the integrated acceptance for the binned e and t
+
+import ROOT
+import pandas as pd
+import numpy as np
+from common_analysis_tools import *
+
+channel = 'pimkpks'
+# channel = 'pipkmks'
+
+df = pd.DataFrame()
+
+acceptance_list = []
+e_list = []
+t_list = []
+
+for e in range(7, 11):
+    for t in range(1, 8):
+        acceptance_spring = get_binned_integrated_phasespace_acceptance(channel, 'spring', e, t, range_lower=1.2, range_upper=1.5)
+        acceptance_fall = get_binned_integrated_phasespace_acceptance(channel, 'fall', e, t, range_lower=1.2, range_upper=1.5)
+        acceptance_2017 = get_binned_integrated_phasespace_acceptance(channel, '2017', e, t, range_lower=1.2, range_upper=1.5)
+        
+        lumi_spring = get_luminosity('spring', e-0.5, e+0.5)
+        lumi_fall = get_luminosity('fall', e-0.5, e+0.5)
+        lumi_2017 = get_luminosity('2017', e-0.5, e+0.5)
+        lumi_total = lumi_spring + lumi_fall + lumi_2017
+
+        avg_acceptance = (acceptance_spring*lumi_spring + acceptance_fall*lumi_fall + acceptance_2017*lumi_2017)/lumi_total
+
+        acceptance_list.append(avg_acceptance)
+        e_list.append(e)
+        t_bin_middle = (t_cut_dict[t][1] + t_cut_dict[t][1]) / 2
+        t_list.append(t_bin_middle)
+
+df['acceptance'] = acceptance_list
+df['beam_energy'] = e_list
+df['t_bin_middle'] = t_list
+
+# write df to a csv 
+df.to_csv(f'/work/halld/home/viducic/data/fit_params/{channel}/binned_integrated_acceptance.csv', index=False)
+
