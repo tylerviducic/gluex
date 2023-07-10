0dc69e2f5cfd71e12f179b8f1114270b46c85ea6
Tyler <tyler.viducic@gmail.com>
2023-07-10 17:02:44 -0400

    added method from common analysis tools

--- scripts/fitting/roofit/f1_resolution.py
+++ scripts/fitting/roofit/f1_resolution.py
@@ -4,77 +4,80 @@ import ROOT
 from common_analysis_tools import *
 
 channel = 'pimkpks'
+cut = 'all'
 
 if channel == 'pipkmks' :
     all_cut = KSTAR_ALL_CUT_PIPKMKS
 elif channel == 'pimkpks' :
     all_cut = KSTAR_ALL_CUT_PIMKPKS
 
-def get_acceptance_corrected_signal_mc(channel, run_period, n_bins):
-    file_and_tree = get_flat_file_and_tree(channel, run_period, 'signal')
-    signal_df = ROOT.RDataFrame(file_and_tree[1], file_and_tree[0]) 
-    recon_phasespace_file_and_tree = get_flat_file_and_tree(channel, run_period, 'phasespace')
-    thrown_phasespace_file_and_tree = get_flat_thrown_file_and_tree(channel, run_period, phasespace=True)
-    recon_df = ROOT.RDataFrame(recon_phasespace_file_and_tree[1], recon_phasespace_file_and_tree[0])
-    thrown_file = ROOT.TFile.Open(thrown_phasespace_file_and_tree[0], 'READ')
-    print(thrown_phasespace_file_and_tree[0])
+# def get_acceptance_corrected_signal_mc(channel, run_period, n_bins):
+#     file_and_tree = get_flat_file_and_tree(channel, run_period, 'signal')
+#     signal_df = ROOT.RDataFrame(file_and_tree[1], file_and_tree[0]) 
+#     recon_phasespace_file_and_tree = get_flat_file_and_tree(channel, run_period, 'phasespace')
+#     thrown_phasespace_file_and_tree = get_flat_thrown_file_and_tree(channel, run_period, phasespace=True)
+#     recon_df = ROOT.RDataFrame(recon_phasespace_file_and_tree[1], recon_phasespace_file_and_tree[0])
+#     thrown_file = ROOT.TFile.Open(thrown_phasespace_file_and_tree[0], 'READ')
+#     print(thrown_phasespace_file_and_tree[0])
 
-    signal_df = signal_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
-    # reduce signal_df to 10% of it's size
-    signal_df = signal_df.Range(0, int(signal_df.Count().GetValue() / 250))
-    recon_df = recon_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
+#     signal_df = signal_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
+#     # reduce signal_df to 10% of it's size
+#     signal_df = signal_df.Range(0, int(signal_df.Count().GetValue() / 250))
+#     recon_df = recon_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
 
-    signal_hist = signal_df.Histo1D((f'data_hist_{run_period}', f'data_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
-    recon_hist = recon_df.Histo1D((f'recon_hist_{run_period}', f'recon_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
-    thrown_hist_name = channel + f'_f1_res_{n_bins};1'
-    thrown_hist = thrown_file.Get(thrown_hist_name)
+#     signal_hist = signal_df.Histo1D((f'data_hist_{run_period}', f'data_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
+#     recon_hist = recon_df.Histo1D((f'recon_hist_{run_period}', f'recon_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
+#     thrown_hist_name = channel + f'_f1_res_{n_bins};1'
+#     thrown_hist = thrown_file.Get(thrown_hist_name)
 
-    signal_hist.Sumw2()
-    recon_hist.Sumw2()
-    thrown_hist.Sumw2()
+#     signal_hist.Sumw2()
+#     recon_hist.Sumw2()
+#     thrown_hist.Sumw2()
 
-    acceptance_hist = recon_hist.Clone()
-    acceptance_hist.Divide(thrown_hist)
+#     acceptance_hist = recon_hist.Clone()
+#     acceptance_hist.Divide(thrown_hist)
         
-    ac_signal_hist = signal_hist.Clone()
-    ac_signal_hist.Divide(acceptance_hist)
+#     ac_signal_hist = signal_hist.Clone()
+#     ac_signal_hist.Divide(acceptance_hist)
 
-    ac_signal_hist.SetDirectory(0)
+#     ac_signal_hist.SetDirectory(0)
 
-    # c = ROOT.TCanvas()
-    # c.Divide(3, 1)
-    # c.cd(1)
-    # signal_hist.Draw()
-    # # c.cd(2)
-    # # recon_hist.Draw()
-    # c.cd(2)
-    # acceptance_hist.Draw()
-    # c.cd(3)
-    # ac_signal_hist.Draw()
-    # c.Update()
-    # input('press enter to continue')
-    # c.Clear()
-    return ac_signal_hist
+#     # c = ROOT.TCanvas()
+#     # c.Divide(3, 1)
+#     # c.cd(1)
+#     # signal_hist.Draw()
+#     # # c.cd(2)
+#     # # recon_hist.Draw()
+#     # c.cd(2)
+#     # acceptance_hist.Draw()
+#     # c.cd(3)
+#     # ac_signal_hist.Draw()
+#     # c.Update()
+#     # input('press enter to continue')
+#     # c.Clear()
+#     return ac_signal_hist
 
-n_bins = 30
-ac_signal_hist_spring = get_acceptance_corrected_signal_mc(channel, 'spring', n_bins)
-ac_signal_hist_fall = get_acceptance_corrected_signal_mc(channel, 'fall', n_bins)
-ac_signal_hist_2017 = get_acceptance_corrected_signal_mc(channel, '2017', n_bins)
+# n_bins = 30
+# ac_signal_hist_spring = get_acceptance_corrected_signal_mc(channel, 'spring', n_bins)
+# ac_signal_hist_fall = get_acceptance_corrected_signal_mc(channel, 'fall', n_bins)
+# ac_signal_hist_2017 = get_acceptance_corrected_signal_mc(channel, '2017', n_bins)
 
-lumi_spring = get_luminosity('spring')
-lumi_fall = get_luminosity('fall')
-lumi_2017 = get_luminosity('2017')
-lumi_total = lumi_spring + lumi_fall + lumi_2017
+# lumi_spring = get_luminosity('spring')
+# lumi_fall = get_luminosity('fall')
+# lumi_2017 = get_luminosity('2017')
+# lumi_total = lumi_spring + lumi_fall + lumi_2017
 
-ac_signal_hist_total = ac_signal_hist_spring.Clone()
-ac_signal_hist_total.Scale(lumi_spring / lumi_total)
-ac_signal_hist_total.Add(ac_signal_hist_fall, lumi_fall / lumi_total)
-ac_signal_hist_total.Add(ac_signal_hist_2017, lumi_2017 / lumi_total)
+# ac_signal_hist_total = ac_signal_hist_spring.Clone()
+# ac_signal_hist_total.Scale(lumi_spring / lumi_total)
+# ac_signal_hist_total.Add(ac_signal_hist_fall, lumi_fall / lumi_total)
+# ac_signal_hist_total.Add(ac_signal_hist_2017, lumi_2017 / lumi_total)
 
-for i in range(1, ac_signal_hist_total.GetNbinsX() + 1):
-    my_error = propogate_error_addition([ac_signal_hist_spring.GetBinError(i), ac_signal_hist_fall.GetBinError(i), ac_signal_hist_2017.GetBinError(i)])
-    sumw2_error = ac_signal_hist_total.GetBinError(i)
-    print(f'bin {i}: my_error = {my_error}, sumw2_error = {sumw2_error}')
+ac_signal_hist_total = get_integrated_gluex1_acceptance_corrected_signal_mc(channel, cut)
+
+# for i in range(1, ac_signal_hist_total.GetNbinsX() + 1):
+#     my_error = propogate_error_addition([ac_signal_hist_spring.GetBinError(i), ac_signal_hist_fall.GetBinError(i), ac_signal_hist_2017.GetBinError(i)])
+#     sumw2_error = ac_signal_hist_total.GetBinError(i)
+#     print(f'bin {i}: my_error = {my_error}, sumw2_error = {sumw2_error}')
 
 m_kkpi = ROOT.RooRealVar("m_kkpi", "m_kkpi", 1.2, 1.5)
 range_min = 1.2
