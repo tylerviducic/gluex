b8c1564f788382618c977d96ee704a9c0fe74fbd
Tyler <tyler.viducic@gmail.com>
2023-07-10 17:46:20 -0400

    reworked to control number of bins and scalefactor

--- scripts/fitting/roofit/f1_resolution.py
+++ scripts/fitting/roofit/f1_resolution.py
@@ -11,53 +11,52 @@ if channel == 'pipkmks' :
 elif channel == 'pimkpks' :
     all_cut = KSTAR_ALL_CUT_PIMKPKS
 
-# def get_acceptance_corrected_signal_mc(channel, run_period, n_bins):
-#     file_and_tree = get_flat_file_and_tree(channel, run_period, 'signal')
-#     signal_df = ROOT.RDataFrame(file_and_tree[1], file_and_tree[0]) 
-#     recon_phasespace_file_and_tree = get_flat_file_and_tree(channel, run_period, 'phasespace')
-#     thrown_phasespace_file_and_tree = get_flat_thrown_file_and_tree(channel, run_period, phasespace=True)
-#     recon_df = ROOT.RDataFrame(recon_phasespace_file_and_tree[1], recon_phasespace_file_and_tree[0])
-#     thrown_file = ROOT.TFile.Open(thrown_phasespace_file_and_tree[0], 'READ')
-#     print(thrown_phasespace_file_and_tree[0])
+def get_acceptance_corrected_signal_mc(channel, run_period, n_bins):
+    file_and_tree = get_flat_file_and_tree(channel, run_period, 'signal')
+    signal_df = ROOT.RDataFrame(file_and_tree[1], file_and_tree[0]) 
+    recon_phasespace_file_and_tree = get_flat_file_and_tree(channel, run_period, 'phasespace')
+    thrown_phasespace_file_and_tree = get_flat_thrown_file_and_tree(channel, run_period, phasespace=True)
+    recon_df = ROOT.RDataFrame(recon_phasespace_file_and_tree[1], recon_phasespace_file_and_tree[0])
+    thrown_file = ROOT.TFile.Open(thrown_phasespace_file_and_tree[0], 'READ')
+    # print(thrown_phasespace_file_and_tree[0])
 
-#     signal_df = signal_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
-#     # reduce signal_df to 10% of it's size
-#     signal_df = signal_df.Range(0, int(signal_df.Count().GetValue() / 250))
-#     recon_df = recon_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
+    signal_df = signal_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
+    # reduce signal_df size
+    # signal_df = signal_df.Range(0, int(signal_df.Count().GetValue() / 250))
+    recon_df = recon_df.Filter(all_cut).Filter(T_RANGE).Filter(BEAM_RANGE)
 
-#     signal_hist = signal_df.Histo1D((f'data_hist_{run_period}', f'data_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
-#     recon_hist = recon_df.Histo1D((f'recon_hist_{run_period}', f'recon_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
-#     thrown_hist_name = channel + f'_f1_res_{n_bins};1'
-#     thrown_hist = thrown_file.Get(thrown_hist_name)
+    signal_hist = signal_df.Histo1D((f'data_hist_{run_period}', f'data_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
+    recon_hist = recon_df.Histo1D((f'recon_hist_{run_period}', f'recon_hist_{run_period}', n_bins, 1.2, 1.5), f'{channel}_m').GetValue()
+    thrown_hist_name = channel + f'_f1_res_{n_bins};1'
+    thrown_hist = thrown_file.Get(thrown_hist_name)
 
-#     signal_hist.Sumw2()
-#     recon_hist.Sumw2()
-#     thrown_hist.Sumw2()
+    signal_hist.Sumw2()
+    recon_hist.Sumw2()
+    thrown_hist.Sumw2()
 
-#     acceptance_hist = recon_hist.Clone()
-#     acceptance_hist.Divide(thrown_hist)
+    acceptance_hist = recon_hist.Clone()
+    acceptance_hist.Divide(thrown_hist)
         
-#     ac_signal_hist = signal_hist.Clone()
-#     ac_signal_hist.Divide(acceptance_hist)
+    ac_signal_hist = signal_hist.Clone()
+    ac_signal_hist.Divide(acceptance_hist)
 
-#     ac_signal_hist.SetDirectory(0)
+    ac_signal_hist.SetDirectory(0)
 
-#     # c = ROOT.TCanvas()
-#     # c.Divide(3, 1)
-#     # c.cd(1)
-#     # signal_hist.Draw()
-#     # # c.cd(2)
-#     # # recon_hist.Draw()
-#     # c.cd(2)
-#     # acceptance_hist.Draw()
-#     # c.cd(3)
-#     # ac_signal_hist.Draw()
-#     # c.Update()
-#     # input('press enter to continue')
-#     # c.Clear()
-#     return ac_signal_hist
+    # c = ROOT.TCanvas()
+    # c.Divide(3, 1)
+    # c.cd(1)
+    # signal_hist.Draw()
+    # # c.cd(2)
+    # # recon_hist.Draw()
+    # c.cd(2)
+    # acceptance_hist.Draw()
+    # c.cd(3)
+    # ac_signal_hist.Draw()
+    # c.Update()
+    # input('press enter to continue')
+    # c.Clear()
+    return ac_signal_hist
 
-# n_bins = 30
 # ac_signal_hist_spring = get_acceptance_corrected_signal_mc(channel, 'spring', n_bins)
 # ac_signal_hist_fall = get_acceptance_corrected_signal_mc(channel, 'fall', n_bins)
 # ac_signal_hist_2017 = get_acceptance_corrected_signal_mc(channel, '2017', n_bins)
@@ -72,7 +71,10 @@ elif channel == 'pimkpks' :
 # ac_signal_hist_total.Add(ac_signal_hist_fall, lumi_fall / lumi_total)
 # ac_signal_hist_total.Add(ac_signal_hist_2017, lumi_2017 / lumi_total)
 
-ac_signal_hist_total = get_integrated_gluex1_acceptance_corrected_signal_mc(channel, cut)
+n_bins = 300
+scale_factor = 10
+
+acc_cor_signal_mc_hist_total = get_integrated_acceptance_corrected_signal_mc_for_resolution_fitting(channel, n_bins, cut, scale_factor=scale_factor)
 
 # for i in range(1, ac_signal_hist_total.GetNbinsX() + 1):
 #     my_error = propogate_error_addition([ac_signal_hist_spring.GetBinError(i), ac_signal_hist_fall.GetBinError(i), ac_signal_hist_2017.GetBinError(i)])
@@ -91,7 +93,7 @@ width.setConstant(ROOT.kTRUE)
 mean.setConstant(ROOT.kTRUE)
 
 
-dh = ROOT.RooDataHist('dh', 'dh', ROOT.RooArgList(m_kkpi), ac_signal_hist_total)
+dh = ROOT.RooDataHist('dh', 'dh', ROOT.RooArgList(m_kkpi), acc_cor_signal_mc_hist_total)
 
 func = ROOT.RooVoigtian('func', 'func', m_kkpi, mean, width, sigma)
 chi2_var = func.createChi2(dh)
@@ -99,7 +101,7 @@ fit_result = func.chi2FitTo(dh, ROOT.RooFit.Save(), ROOT.RooFit.Range("fit_range
 fit_result = func.chi2FitTo(dh, ROOT.RooFit.Save())
 
 chi2_val = chi2_var.getVal()
-n_bins = ac_signal_hist_total.GetNbinsX()
+n_bins = acc_cor_signal_mc_hist_total.GetNbinsX()
 print("n_bins = " + str(n_bins))
 ndf = n_bins - (fit_result.floatParsFinal().getSize() - fit_result.constPars().getSize())
 chi2_per_ndf = chi2_val / ndf
@@ -118,7 +120,7 @@ chi2ndf = frame.chiSquare(npar)
 
 frame.Draw()
 
-input('press enter to continue')
+# input('press enter to continue')
 
 pullDist = ROOT.TH1I("pullDist", "pullDist", 3, 0, 3)
 for i in range(0, pullHist.GetN()):
