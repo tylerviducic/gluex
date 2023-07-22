385bf8b55f4428a92676c5b6f63773fddd03439d
Tyler <tyler.viducic@gmail.com>
2023-07-22 12:47:47 -0400

    all 4 kstars added

--- my_library/thesis_plotter_library.py
+++ my_library/thesis_plotter_library.py
@@ -9,13 +9,14 @@ import os
 
 def get_dataframe(channel='pipkmks', run_period='spring', datatype='data'):
     file_and_tree = ct.get_flat_file_and_tree(channel, run_period, datatype, filtered=False)
-
     df = ROOT.RDataFrame(file_and_tree[1], file_and_tree[0])
     return ct.define_columns(df, channel)
+
 
 def save_plot(canvas, filename):
     path_to_thesis_plots = '/work/halld/home/viducic/plots/thesis/'
     canvas.SaveAs(path_to_thesis_plots + filename + '.png')
+
 
 def build_legend(histograms: list, x1=0.7, y1=0.7, x2=0.9, y2=0.9, labels: list=[]):
     if len(labels) != len(histograms):
@@ -26,11 +27,11 @@ def build_legend(histograms: list, x1=0.7, y1=0.7, x2=0.9, y2=0.9, labels: list=
         legend.AddEntry(hist, labels[histograms.index(hist)], 'l')
     return legend
 
+
 def get_ks_before_after_cut(bin_low=0.3, bin_high=0.7, nbins=500):
     os.nice(18)
     ROOT.EnableImplicitMT()
     df = get_dataframe()
-
     df.Filter(ct.MX2_PPIPKMKS_CUT)
 
     hist_no_cut = df.Histo1D(('ks_m', 'ks_m', nbins, bin_low, bin_high), 'ks_m')
@@ -47,6 +48,7 @@ def get_ks_before_after_cut(bin_low=0.3, bin_high=0.7, nbins=500):
     hist_no_cut.SetDirectory(0)
     return hist_no_cut.GetValue(), hist_cut.GetValue()
 
+
 def get_mpipi_vs_pathlength_sig(bin_lowx=0.3, bin_highx=0.7, bin_lowy=0, bin_highy=10, nbinsx=200, nbinsy=200):
     os.nice(18)
     ROOT.EnableImplicitMT()
@@ -59,6 +61,7 @@ def get_mpipi_vs_pathlength_sig(bin_lowx=0.3, bin_highx=0.7, bin_lowy=0, bin_hig
     hist.GetYaxis().SetTitle("Pathlength Signifigance")
     hist.SetDirectory(0)
     return hist.GetValue()
+
 
 def result_of_p_p_cut(nbins= 500, xlow=0.0, xhigh=2.0):
     df = get_dataframe()
@@ -77,6 +80,7 @@ def result_of_p_p_cut(nbins= 500, xlow=0.0, xhigh=2.0):
     hist_t_mand.SetDirectory(0)
     return hist_t_mand.GetValue(), hist_cut_tmand.GetValue()
 
+
 def plot_mx2_all(bin_low=-0.05, bin_high=0.05, nbins=500):
     data_file = '/work/halld/home/viducic/data/pipkmks/data/bestX2/pipkmks_flat_bestX2_2018_spring.root'
     file_treename = 'pipkmks__B4_M16'
@@ -91,6 +95,7 @@ def plot_mx2_all(bin_low=-0.05, bin_high=0.05, nbins=500):
     hist.SetLineColor(ROOT.TColor.GetColor(ct.COLORBLIND_HEX_DICT['blue']))
     hist.SetDirectory(0)
     return hist 
+
 
 def plot_pipkmks_phasespace_with_cuts():
     file_and_tree = ct.get_flat_phasespace_file_and_tree('pipkmks', 'spring')
@@ -107,6 +112,7 @@ def plot_pipkmks_phasespace_with_cuts():
     c.Update()
     input("Press Enter to continue...")
 
+
 def plot_delta(bin_low=1.0, bin_high=2.5, nbins=200):
     df = get_dataframe()
     df = df.Filter(ct.MX2_PPIPKMKS_CUT).Filter(ct.KS_PATHLENGTH_CUT).Filter(ct.KS_MASS_CUT).Filter(ct.P_P_CUT)
@@ -118,6 +124,7 @@ def plot_delta(bin_low=1.0, bin_high=2.5, nbins=200):
     hist_delta.SetDirectory(0)
     return hist_delta.GetValue()
 
+
 def plot_nstar(bin_low=1.0, bin_high=2.5, nbins=200):
     df = get_dataframe(channel='pimkpks')
     df = df.Filter(ct.MX2_PPIMKPKS_CUT).Filter(ct.KS_PATHLENGTH_CUT).Filter(ct.KS_MASS_CUT).Filter(ct.P_P_CUT)
@@ -128,6 +135,7 @@ def plot_nstar(bin_low=1.0, bin_high=2.5, nbins=200):
     hist_nstar.SetLineColor(ROOT.TColor.GetColor(ct.COLORBLIND_HEX_DICT['blue']))
     hist_nstar.SetDirectory(0)
     return hist_nstar.GetValue()
+
 
 def plot_baryons():
     hist_delta = plot_delta()
@@ -153,6 +161,7 @@ def plot_ksp(channel, bin_low=1.4, bin_high=2.5, nbins=200):
     hist_ksp.SetDirectory(0)
     return hist_ksp.GetValue()
 
+
 def plot_kmp(bin_low=1.4, bin_high=2.5, nbins=200):
     df = get_dataframe()
     df = df.Filter(ct.MX2_PPIPKMKS_CUT).Filter(ct.KS_PATHLENGTH_CUT).Filter(ct.KS_MASS_CUT).Filter(ct.P_P_CUT).Filter(ct.PPIP_MASS_CUT)
@@ -163,6 +172,7 @@ def plot_kmp(bin_low=1.4, bin_high=2.5, nbins=200):
     hist_kmp.SetLineColor(ROOT.TColor.GetColor(ct.COLORBLIND_HEX_DICT['blue']))
     hist_kmp.SetDirectory(0)
     return hist_kmp.GetValue()
+
 
 def plot_kpp(bin_low=1.4, bin_high=2.5, nbins=200):
     df = get_dataframe('pimkpks')
@@ -183,6 +193,50 @@ def plot_lambdas():
     hist_ksp_pimkpks = plot_ksp('pimkpks')
     return hist_kmp, hist_kpp, hist_ksp_pipkmks, hist_ksp_pimkpks
     
+
+def plot_neutral_kstar(channel, nbins=200, xlow=0.5, xhigh=2.0):
+    df = get_dataframe(channel)
+    df = df.Filter(ct.KS_PATHLENGTH_CUT).Filter(ct.KS_MASS_CUT).Filter(ct.P_P_CUT)
+    if channel == 'pimkpks':
+        df = df.Filter(ct.MX2_PPIMKPKS_CUT).Filter(ct.PPIM_MASS_CUT).Filter(ct.KSP_MASS_CUT)
+        hist_neutral_kstar = df.Histo1D(('kstar_zero_m', 'M(K^{+}#pi^{-}) GeV', nbins, xlow, xhigh), 'kppim_m')
+        hist_neutral_kstar.SetTitle("M(K^{+}#pi^{-}) GeV")
+    elif channel == 'pipkmks':
+        df = df.Filter(ct.MX2_PPIPKMKS_CUT).Filter(ct.PPIP_MASS_CUT).Filter(ct.KMP_MASS_CUT)
+        hist_neutral_kstar = df.Histo1D(('kstar_zero)_m', 'M(K^{-}#pi^{+}) GeV', nbins, xlow, xhigh), 'kmpip_m')
+        hist_neutral_kstar.SetTitle("M(K^{-}#pi^{+}) GeV")
+    hist_neutral_kstar.GetXaxis().SetTitle(hist_neutral_kstar.GetTitle())
+    hist_neutral_kstar.GetYaxis().SetTitle(f"Counts/{1000*((xhigh-xlow)/nbins):.2f} MeV")
+    hist_neutral_kstar.SetLineColor(ROOT.TColor.GetColor(ct.COLORBLIND_HEX_DICT['blue']))
+    hist_neutral_kstar.SetDirectory(0)
+    return hist_neutral_kstar.GetValue()
+
+
+def plot_charged_kstar(channel, nbins=200, xlow=0.5, xhigh=2.0):
+    df = get_dataframe(channel)
+    df = df.Filter(ct.KS_PATHLENGTH_CUT).Filter(ct.KS_MASS_CUT).Filter(ct.P_P_CUT)
+    if channel == 'pimkpks':
+        df = df.Filter(ct.MX2_PPIMKPKS_CUT).Filter(ct.PPIM_MASS_CUT).Filter(ct.KSP_MASS_CUT)
+        hist_charged_kstar = df.Histo1D(('kstar_minus_m', 'M(K_{S}#pi^{-}) GeV', nbins, xlow, xhigh), 'kspim_m')
+        hist_charged_kstar.SetTitle("M(K_{S}#pi^{+}) GeV")
+    elif channel == 'pipkmks':
+        df = df.Filter(ct.MX2_PPIPKMKS_CUT).Filter(ct.PPIP_MASS_CUT).Filter(ct.KMP_MASS_CUT)
+        hist_charged_kstar = df.Histo1D(('kstar_plus_m', 'M(K_{S}#pi^{+}) GeV', nbins, xlow, xhigh), 'kspip_m')
+        hist_charged_kstar.SetTitle("M(K_{S}#pi^{-}) GeV")
+    hist_charged_kstar.GetXaxis().SetTitle(hist_charged_kstar.GetTitle())
+    hist_charged_kstar.GetYaxis().SetTitle(f"Counts/{1000*((xhigh-xlow)/nbins):.2f} MeV")
+    hist_charged_kstar.SetLineColor(ROOT.TColor.GetColor(ct.COLORBLIND_HEX_DICT['blue']))
+    hist_charged_kstar.SetDirectory(0)
+    return hist_charged_kstar.GetValue()
+
+
+def plot_kstars():
+    hist_charged_kstar_pipkmks = plot_charged_kstar('pipkmks')
+    hist_charged_kstar_pimkpks = plot_charged_kstar('pimkpks')
+    hist_neutral_kstar_pipkmks = plot_neutral_kstar('pipkmks')
+    hist_neutral_kstar_pimkpks = plot_neutral_kstar('pimkpks')
+    return hist_charged_kstar_pipkmks, hist_charged_kstar_pimkpks, hist_neutral_kstar_pipkmks, hist_neutral_kstar_pimkpks
+
 
 if __name__ == "__main__":
     ROOT.gStyle.SetOptStat(0)
