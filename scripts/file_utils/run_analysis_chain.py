e995cef0b31eaaf8908453d3e1fee416ca99f1c4
Tyler <tyler.viducic@gmail.com>
2023-07-27 21:27:33 -0400

    lines to time

--- scripts/file_utils/run_analysis_chain.py
+++ scripts/file_utils/run_analysis_chain.py
@@ -1,6 +1,8 @@
 # script to execute pipkmks data analysis over all 3 run periods
 
 import os
+from my_library.flat_analysis import run_analysis
+import time
 
 #TODO STATUS BAR OR TIMER OR BOTH
 #TODO options for smaller-scale running (maybe pass lists as arguments?)
@@ -20,15 +22,21 @@ def run_thrown_analysis(channel: str, run_period: str, data_type: str):
     os.system(command)
 
 if __name__ == '__main__':
+
     channels = ['pipkmks', 'pimkpks']
     run_periods = ['spring', 'fall', '2017']
     recon_data_types = ['data', 'signal', 'phasespace']
     thrown_data_types = ['signal', 'phasespace']
 
+
     for channel in channels:
         for data_type in recon_data_types:
             for run_period in run_periods:
-                run_recon_analysis(channel, run_period, data_type)
-        for data_type in thrown_data_types:
-            for run_period in run_periods:
-                run_thrown_analysis(channel, run_period, data_type)
+                start = time.time()
+                run_analysis(channel, run_period, data_type)
+                end = time.time()
+                print(f'Analysis for {channel} {data_type} {run_period} took {end-start} seconds')
+
+        # for data_type in thrown_data_types:
+        #     for run_period in run_periods:
+        #         run_thrown_analysis(channel, run_period, data_type)
