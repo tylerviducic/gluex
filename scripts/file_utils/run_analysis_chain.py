127cdf954c56d718ea63c2b9057a0bfc89b90230
Tyler <tyler.viducic@gmail.com>
2023-07-28 18:32:06 -0400

    should run over thrown now too

--- scripts/file_utils/run_analysis_chain.py
+++ scripts/file_utils/run_analysis_chain.py
@@ -23,7 +23,9 @@ def run_thrown_analysis(channel: str, run_period: str, data_type: str):
 
 if __name__ == '__main__':
 
+    overall_start = time.time()
     channels = ['pipkmks', 'pimkpks']
+    # channels = ['pimkpks']
     run_periods = ['spring', 'fall', '2017']
     recon_data_types = ['data', 'signal', 'phasespace']
     thrown_data_types = ['signal', 'phasespace']
@@ -32,11 +34,22 @@ if __name__ == '__main__':
     for channel in channels:
         for data_type in recon_data_types:
             for run_period in run_periods:
+                print(f'running analysis for {channel} {data_type} {run_period}')
                 start = time.time()
                 run_analysis(channel, run_period, data_type)
                 end = time.time()
                 print(f'Analysis for {channel} {data_type} {run_period} took {end-start} seconds')
+                if data_type in thrown_data_types:
+                    print(f'running thrown analysis for {channel} {data_type} {run_period}')
+                    thrown_start = time.time()
+                    run_analysis(channel, run_period, data_type, thrown=True)
+                    thrown_end = time.time()
+                    print(f'Thrown analysis for {channel} {data_type} {run_period} took {thrown_end-thrown_start} seconds')
+
 
         # for data_type in thrown_data_types:
         #     for run_period in run_periods:
         #         run_thrown_analysis(channel, run_period, data_type)
+
+    overall_end = time.time()
+    print(f'Overall analysis took {overall_end-overall_start} seconds')
