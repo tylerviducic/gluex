93ee31aa1243bd433708d0ec1208405a4e898414
Tyler <tyler.viducic@gmail.com>
2023-08-23 18:29:04 -0400

    removed print statement

--- mc/analysis/pipkmks/RunF1MC_KsKmPip.C
+++ mc/analysis/pipkmks/RunF1MC_KsKmPip.C
@@ -1,22 +1,10 @@
 void RunF1MC_KsKmPip(string filename){
 
-    // gROOT->ProcessLine("TChain* ch = new TChain(\"pipkmks__ks_pippim__B4_M16_Tree\")");
-    // TChain* ch = new TChain("pipkmks__ks_pippim__B4_M16_Tree");
-    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/04*"); //spring 18
-    // gROOT->ProcessLine("ch->Add(\"/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/04*/*.root"); //spring 18
-    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/05*") //fall 18
-    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/03*") // 17 spring
-    // gROOT->ProcessLine(".x $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C");
-    // gROOT->ProcessLine("DPROOFLiteManager::Process_Chain(ch, \"DSelector_mc_pipkmks_flat.C++\", 6)");
     #include "TProof.h"
     #include "TProofDebug.h"
     R__LOAD_LIBRARY(libDSelector);
 
     TChain* ch = new TChain("pipkmks__ks_pippim__B4_M16_Tree");
-    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/04*/*"); //spring 18
-    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/spring_2018/*"); //spring 18
-    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/fall_2018/*"); //spring 18
-    cout<<filename<<endl;
     char *cstr = new char[filename.length() + 1];
     strcpy(cstr, filename.c_str());
     ch->Add(cstr);
@@ -50,3 +38,18 @@ void RunF1MC_KsKmPip(string filename){
         // // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/pipkmks_s18_100m_v16_rt_t29_3237/root/trees/tree_pipkmks__ks_pippim__B4_M16_gen_amp/*.root\")"); //spring
     // // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/pipkmks_f18_100m_v16_rt_t29_3238/root/trees/tree_pipkmks__ks_pippim__B4_M16_gen_amp/*.root\")"); //fall
     // //gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/cache/halld/gluex_simulations/REQUESTED_MC/pipkmks_f18_100m_v16_rt_t29_3239/root/trees/tree_pipkmks__ks_pippim__B4_M16_gen_amp/*.root\")"); //THIS IS 2017 THERE WAS A NAMING ERROR
+
+
+        // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/04*/*"); //spring 18
+    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/spring_2018/*"); //spring 18
+    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/fall_2018/*"); //spring 18
+
+
+    // gROOT->ProcessLine("TChain* ch = new TChain(\"pipkmks__ks_pippim__B4_M16_Tree\")");
+    // TChain* ch = new TChain("pipkmks__ks_pippim__B4_M16_Tree");
+    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/04*"); //spring 18
+    // gROOT->ProcessLine("ch->Add(\"/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/04*/*.root"); //spring 18
+    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/05*") //fall 18
+    // ch->Add("/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/03*") // 17 spring
+    // gROOT->ProcessLine(".x $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C");
+    // gROOT->ProcessLine("DPROOFLiteManager::Process_Chain(ch, \"DSelector_mc_pipkmks_flat.C++\", 6)");
