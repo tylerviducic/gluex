98ff7840be3a62e94547147f1df2f587e7da6e73
Tyler <tyler.viducic@gmail.com>
2023-08-24 09:43:02 -0400

    run over pi-k+

--- mc/analysis/pimkpks/RunF1MC_KsKpPim_CorrectRF.C
+++ mc/analysis/pimkpks/RunF1MC_KsKpPim_CorrectRF.C
@@ -1,14 +1,27 @@
 // pimkpks__ks_pippim__B4_M16_Tree
-void RunF1MC_KsKpPim_CorrectRF(){
+void RunF1MC_KsKpPim_CorrectRF(string filename){
 
-    gROOT->ProcessLine("TChain* ch = new TChain(\"pimkpks__ks_pippim__B4_M16_Tree\")");
-    // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_03*.root\")"); // 2017
-    gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_04*.root\")");  // 2018 spring
-    // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_05*.root\")");  // 2018 fall
+    #include "TProof.h"
+    #include "TProofDebug.h"
+    R__LOAD_LIBRARY(libDSelector);
+
+    TChain* ch = new TChain("pimkpks__ks_pippim__B4_M16_Tree");
+    char *cstr = new char[filename.length() + 1];
+    strcpy(cstr, filename.c_str());
+    ch->Add(cstr);
     gROOT->ProcessLine(".x $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C");
-    gROOT->ProcessLine("DPROOFLiteManager::Process_Chain(ch, \"DSelector_mc_pimkpks_flat_correct_rf.C++\", 6)");
+    DPROOFLiteManager *dproof = new DPROOFLiteManager();
+    dproof->Process_Chain(ch, "DSelector_mc_pimkpks_flat_correct_rf.C++", 6);
 
-} 
+}
+
+
+    // gROOT->ProcessLine("TChain* ch = new TChain(\"pimkpks__ks_pippim__B4_M16_Tree\")");
+    // // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_03*.root\")"); // 2017
+    // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_04*.root\")");  // 2018 spring
+    // // gROOT->ProcessLine("ch->Add(\"/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/combined_runs/tree_pimkpks__ks_pippim__B4_M16_05*.root\")");  // 2018 fall
+    // gROOT->ProcessLine(".x $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C");
+    // gROOT->ProcessLine("DPROOFLiteManager::Process_Chain(ch, \"DSelector_mc_pimkpks_flat_correct_rf.C++\", 6)");
 
 // for phasespace
 //file paths for whenever I get around to writing this
