6a0964398dc823c8f1b3f08102b8e0526582ecdd
Tyler <tyler.viducic@gmail.com>
2023-09-07 17:55:41 -0400

    rename signal run file, add phasespace

---
+++ mc/analysis/pimkpks/run_dselector_all_gluex1_signal.py
@@ -0,0 +1,35 @@
+import os
+
+directories_pimkpks = {
+    "spring": "spring_2018",
+    "fall": "fall_2018",
+    "2017": "2017"
+}
+
+filenames_pimkpks = {
+    "spring": "2018_spring",
+    "fall": "2018_fall",
+    "2017": "2017"
+}
+
+
+def run_selector_macro(run_period: str):
+    top_dir =  '/lustre19/expphy/volatile/halld/home/viducic/pimkpks_mc/signal/tree_pimkpks__ks_pippim__B4_M16/'
+    print(f"RUNNING DSELECTOR FOR PI+K-Ks OVER {run_period.upper()} SIGNAL MC")
+    filepath_for_selector = f'{top_dir}{directories_pimkpks[run_period]}/*'
+    ds_command = f"root -l -b -q \'RunF1MC_KsKpPim_CorrectRF.C(\"{filepath_for_selector}\")\'"
+    print(ds_command)
+    os.system(ds_command)
+
+
+def rename_file(run_period: str):
+    outfile =  '/work/halld/home/viducic/data/pimkpks/mc/signal/pimkpks_flat_bestX2.root'
+    mv_command = f'mv {outfile} /work/halld/home/viducic/data/pimkpks/mc/signal/pimkpks_flat_bestX2_{filenames_pimkpks[run_period]}.root'
+    print(mv_command)
+    os.system(mv_command)
+
+
+if __name__ == "__main__":
+    for run_period in directories_pimkpks:
+        run_selector_macro(run_period)
+        rename_file(run_period)
