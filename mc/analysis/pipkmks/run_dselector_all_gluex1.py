17259b69931540dfd1cf8aee3147d6870ae0fca9
Tyler <tyler.viducic@gmail.com>
2023-09-10 18:44:42 -0400

    run pipkmks dselector files

--- mc/analysis/pipkmks/run_dselector_all_gluex1.py
+++ mc/analysis/pipkmks/run_dselector_all_gluex1.py
@@ -13,17 +13,17 @@ filenames_pipkmks = {
 }
 
 def run_selector_macro(run_period: str):
-    top_dir =  '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_mc/signal/tree_pipkmks__ks_pippim__B4_M16/'
+    top_dir =  '/lustre19/expphy/volatile/halld/home/viducic/pipkmks_mc/phasespace/tree_pipkmks__ks_pippim__B4_M16/'
     print(f"RUNNING DSELECTOR FOR PI+K-Ks OVER {run_period.upper()} SIGNAL MC")
     filepath_for_selector = f'{top_dir}{directories_pipkmks[run_period]}/*'
-    ds_command = f"root -l -b -q \'RunF1MC_KsKmPip.C(\"{filepath_for_selector}\")\'"
+    ds_command = f"root -l -b -q \'RunF1Phasespace_KsKmPip.C(\"{filepath_for_selector}\")\'"
     print(ds_command)
     os.system(ds_command)
 
 
 def rename_file(run_period: str):
-    outfile =  '/work/halld/home/viducic/data/pipkmks/mc/signal/pipkmks_flat_bestX2.root'
-    mv_command = f'mv {outfile} /work/halld/home/viducic/data/pipkmks/mc/signal/pipkmks_flat_bestX2_{filenames_pipkmks[run_period]}.root'
+    outfile =  '/work/halld/home/viducic/data/pipkmks/mc/phasespace/pipkmks_flat_bestX2.root'
+    mv_command = f'mv {outfile} /work/halld/home/viducic/data/pipkmks/mc/phasespace/pipkmks_flat_bestX2_{filenames_pipkmks[run_period]}.root'
     print(mv_command)
     os.system(mv_command)
 
