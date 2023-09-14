fd8895b83cf4119789c7e211a32de340de7be7b9
Tyler <tyler.viducic@gmail.com>
2023-09-14 18:16:21 -0400

    new runscript. its not great but it should work

--- scripts/file_utils/run_ver_test_dselectors.py
+++ scripts/file_utils/run_ver_test_dselectors.py
@@ -1,18 +1,15 @@
 import os
 
 
-filepath = '/work/halld/home/viducic/data/{}/data/ver_test/{}_041000-041008_ver_{}.root'
-outpath = '/work/halld/home/viducic/data/{}/data/ver_test/{}_flat_bestX2_ver_{}.root'
-selector_path = '/work/halld/home/viducic/analysis/{}/RunVerTest.C'
+runfile_datafile_dict = {
+    '/work/halld/home/viducic/analysis/pipkmks/RunVerTest16.C': '/work/halld/home/viducic/data/pipkmks/data/ver_test/pipkmks_041000-041008_ver_16.root',
+    '/work/halld/home/viducic/analysis/pipkmks/RunVerTest23.C': '/work/halld/home/viducic/data/pipkmks/data/ver_test/pipkmks_041000-041008_ver_23.root',
+    '/work/halld/home/viducic/analysis/pimkpks/RunVerTest16.C': '/work/halld/home/viducic/data/pimkpks/data/ver_test/pimkpks_041000-041008_ver_16.root',
+    '/work/halld/home/viducic/analysis/pimkpks/RunVerTest23.C': '/work/halld/home/viducic/data/pimkpks/data/ver_test/pimkpks_041000-041008_ver_23.root'
+}
 
-versions = [16, 23]
-channels = ['pipkmks', 'pimkpks']
 
-for channel in channels:
-    for version in versions:
-        command = f'root -l -b -q \'{selector_path.format(channel)}("{filepath.format(channel, channel, version)}")\''
-        print(command)
-        os.system(command)
-        mv_command = f'mv {channel}_flat_bestX2.root {outpath.format(channel, channel, version)}'
-        print(mv_command)
-        os.system(mv_command)
+for runfile in runfile_datafile_dict:
+    command = f'root -l -b -q \'{runfile}("{runfile_datafile_dict[runfile]}")\''
+    print(command)
+    os.system(command)
