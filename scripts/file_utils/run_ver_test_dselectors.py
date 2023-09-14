dd695d38e85c940d5c5f67f914cbe64199366f9d
Tyler <tyler.viducic@gmail.com>
2023-09-14 18:24:52 -0400

    testing commands

--- scripts/file_utils/run_ver_test_dselectors.py
+++ scripts/file_utils/run_ver_test_dselectors.py
@@ -8,8 +8,12 @@ runfile_datafile_dict = {
     '/work/halld/home/viducic/analysis/pimkpks/RunVerTest23.C': '/work/halld/home/viducic/data/pimkpks/data/ver_test/pimkpks_041000-041008_ver_23.root'
 }
 
+result_filenames = ['pipkmks_v16', 'pipkmks_v23', 'pimkpks_v16', 'pimkpks_v23']
+outfiles = ['pipkmks_flat_bestX2.root,' 'pipkmks_flat_bestX2_ver23.root', 'pimkpks_flat_bestX2.root,' 'pimkpks_flat_bestX2_ver23.root']
 
-for runfile in runfile_datafile_dict:
+for i, runfile in runfile_datafile_dict.keys():
     command = f'root -l -b -q \'{runfile}("{runfile_datafile_dict[runfile]}")\''
     print(command)
-    os.system(command)
+    # os.system(command)
+    outpath = f'/work/halld/home/viducic/data/{outfiles[i].split("_")[0]}/data/ver_test/{result_filenames[i]}.root'
+    mv_command = f'mv {outfiles[i]} {outpath}'
