fedd1f47ab0a191ae5480a74d470811b5b51c391
Tyler <tyler.viducic@gmail.com>
2023-09-19 17:18:25 -0400

    added move command

--- mc/analysis/pimkpks/env_files/run_nstar_dselectors.py
+++ mc/analysis/pimkpks/env_files/run_nstar_dselectors.py
@@ -12,11 +12,12 @@ nstar_dirs = [
     #'nstar_1535_pimkpks_3395' ADD THE COMMA TO PROCEDING LINE WHEN UNCOMMENTING
 ]
 
-ds_output_dir = "/work/halld/home/viducic/data/pimkpks/mc/signal/" #pimkpks_flat_bestX2.root
+ds_output_file = "/work/halld/home/viducic/data/pimkpks/mc/signal/imkpks_flat_bestX2.root" 
 nstar_dir = "/work/halld/home/viducic/data/pimkpks/mc/nstar/"
 
 for nstar_dir in nstar_dirs:
     path_to_files = f'{common_path}{nstar_dir}/root/trees/tree_pimkpks__ks_pippim__B4_M16_genr8/'
     run_command = f'root -l -b -q \'RunNstarMC_KsKpPim.C("{path_to_files}*.root")\''
     print(run_command)
-    mv_command = f'mv '
+    mv_command = f'mv {ds_output_file} {nstar_dir}/nstar_{nstar_dir.split("_")[1]}_flat_bestX2.root'
+    print(mv_command)
