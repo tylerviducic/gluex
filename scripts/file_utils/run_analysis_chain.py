8c79366d7f91a7d4d143f2f295f55cdf71620455
Tyler <tyler.viducic@gmail.com>
2023-09-21 12:33:16 -0400

    add todo

--- scripts/file_utils/run_analysis_chain.py
+++ scripts/file_utils/run_analysis_chain.py
@@ -5,8 +5,8 @@ from my_library.flat_analysis import run_analysis
 
 import time
 
-#TODO STATUS BAR OR TIMER OR BOTH
-#TODO options for smaller-scale running (maybe pass lists as arguments?)
+#TODO:  STATUS BAR OR TIMER OR BOTH
+#TODO:  options for smaller-scale running (maybe pass lists as arguments?)
 
 
 if __name__ == '__main__':
