98af202b0046f0b7fe6686be192acc325db8b98e
Tyler <tyler.viducic@gmail.com>
2023-09-27 17:20:26 -0400

    fitting with an extra bump. works well for pipkmks

--- scripts/fitting/roofit/two_bumps.py
+++ scripts/fitting/roofit/two_bumps.py
@@ -13,7 +13,8 @@ os.nice(18)
 ROOT.EnableImplicitMT(8)
 ROOT.gStyle.SetOptStat(0)
 
-channel = 'pipkmks'
+# channel = 'pipkmks'
+channel = 'pimkpks'
 cut = 'all'
 
 if channel == 'pipkmks' :
@@ -25,28 +26,33 @@ elif channel == 'pimkpks' :
 
 data_hist = ct.get_integrated_gluex1_kstar_corrected_data_hist(channel)
 
-kkpi_low, kkpi_high = 1.15, 1.5
+kkpi_low, kkpi_high = 1.2, 1.5
 m_kkpi = ROOT.RooRealVar("m_kkpi", "m_kkpi", kkpi_low, kkpi_high)
 dh = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(m_kkpi), data_hist)
 
 voight_m = ROOT.RooRealVar("voight_m", "voight_m", 1.285, 1.2, 1.3)
-voight_width = ROOT.RooRealVar("voight_width", "voight_width", 0.023, 0.01, 0.075)
+voight_width = ROOT.RooRealVar("voight_width", "voight_width", 0.0227, 0.01, 0.075)
 voight_sigma = ROOT.RooRealVar("voight_sigma", "voight_sigma", voight_resoltion, 0.01, 0.5)
 voight_sigma.setError(voight_resolution_error)
 voight = ROOT.RooVoigtian("voight", "voight", m_kkpi, voight_m, voight_width, voight_sigma)
 voight_sigma.setConstant(True)
+# voight_m.setConstant(True)
+# voight_width.setConstant(True)
 
-gaus_m = ROOT.RooRealVar("gaus_m", "gaus_m", 1.39, 1.35, 1.43)
-gaus_width = ROOT.RooRealVar("gaus_width", "gaus_width", 0.03, 0.005, 0.075)
+gaus_m = ROOT.RooRealVar("gaus_m", "gaus_m", 1.39, 1.35, 1.41)
+gaus_width = ROOT.RooRealVar("gaus_width", "gaus_width", 0.03, 0.005, 0.05)
 gaus = ROOT.RooGaussian("gaus", "gaus", m_kkpi, gaus_m, gaus_width)
+# gaus_m.setConstant(True)
+# gaus_width.setConstant(True)
 
 ## CHEBYCHEV ##
-bkg_par1 = ROOT.RooRealVar("bkg_par1", "bkg_par1", -2.0, 2.0)
-bkg_par2 = ROOT.RooRealVar("bkg_par2", "bkg_par2", -2.0, 2.0)
-bkg_par3 = ROOT.RooRealVar("bkg_par3", "bkg_par3", -2.0, 2.0)
-bkg_par4 = ROOT.RooRealVar("bkg_par4", "bkg_par4", -2.0, 2.0)
+bkg_par1 = ROOT.RooRealVar("bkg_par1", "bkg_par1", 0.0, 2.0)
+bkg_par2 = ROOT.RooRealVar("bkg_par2", "bkg_par2", 0.0, 2.0)
+bkg_par3 = ROOT.RooRealVar("bkg_par3", "bkg_par3", 0.0, 2.0)
+bkg_par4 = ROOT.RooRealVar("bkg_par4", "bkg_par4", 0.0, 2.0)
 
 bkg = ROOT.RooChebychev("bkg", "bkg", m_kkpi, ROOT.RooArgList(bkg_par1))
+# bkg_par1.setConstant(True)
 
 n_f1 = ROOT.RooRealVar("n_f1", "n_f1", 10000, 0.0, 1000000000)
 n_gaus = ROOT.RooRealVar("n_gaus", "n_gaus", 10000, 0.0, 1000000000)
@@ -54,7 +60,7 @@ n_bkg = ROOT.RooRealVar("n_bkg", "n_bkg", 10000, 0.0, 1000000000)
 
 combined_pdf = ROOT.RooAddPdf('combined_pdf', 'combined_pdf', ROOT.RooArgList(voight, gaus, bkg), ROOT.RooArgList(n_f1, n_gaus, n_bkg))
 
-c2 = ROOT.RooChi2Var(f"c2", f"c2", combined_pdf, dh, ROOT.RooFit.Extended(True), ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2), ROOT.RooFit.Range("fit_range"))
+c2 = ROOT.RooChi2Var(f"c2", f"c2", combined_pdf, dh, ROOT.RooFit.Extended(True), ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2))
 minuit = ROOT.RooMinuit(c2)
 minuit.migrad()
 minuit.minos()
@@ -76,7 +82,7 @@ frame.GetXaxis().SetTitle(f'{title.split(" ")[0]} GeV')
 frame.GetYaxis().SetTitle(f'Counts/10MeV')
 
 dh.plotOn(frame, ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2))
-combined_pdf.plotOn(frame, ROOT.RooFit.Range("fit_range"), ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['red'])))
+combined_pdf.plotOn(frame, ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['red'])))
 pullHist = frame.pullHist()
 combined_pdf.plotOn(frame, ROOT.RooFit.Components("bkg"), ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['purple'])), ROOT.RooFit.LineStyle(ROOT.kDashed))
 combined_pdf.plotOn(frame, ROOT.RooFit.Components("voight"), ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['blue'])))
@@ -92,8 +98,8 @@ for i in range(0, pullHist.GetN()):
 
 ks_test_func = combined_pdf.createHistogram("ks_test_func", m_kkpi)#, ROOT.RooFit.Binning(1000))
 ks_test_data = dh.createHistogram("ks_test_data", m_kkpi)#, ROOT.RooFit.Binning(1000))
-ks_test_data.Scale(1/ks_test_data.Integral())
-ks_test_func.Scale(1/ks_test_func.Integral())
+# ks_test_data.Scale(1/ks_test_data.Integral())
+# ks_test_func.Scale(1/ks_test_func.Integral())
 
 kstest = ks_test_data.KolmogorovTest(ks_test_func)
 print("K-S test = " + str(kstest))
@@ -127,6 +133,7 @@ print(f"f1 mass = {voight_m.getVal() * 1000} +/- {voight_m.getError() * 1000}")
 print(f"f1 width = {voight_width.getVal() * 1000} +/- {voight_width.getError() * 1000}")
 print(f'gaus mean = {gaus_m.getVal() * 1000} +/- {gaus_m.getError() * 1000}')
 print(f'gaus width = {gaus_width.getVal() * 1000} +/- {gaus_width.getError() * 1000}')
+print(f'bkg par1 = {bkg_par1.getVal()} +/- {bkg_par1.getError()}')
 print("chi2 = " + str(c2.getVal()))
 print("ndf = " + str(ndf))
 print("chi2/ndf (manual calculation) = " + str(chi2_per_ndf))
