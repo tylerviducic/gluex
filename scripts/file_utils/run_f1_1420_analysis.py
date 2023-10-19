96fc82a248ab932b805492edeb5ba7d25af45e53
Tyler <tyler.viducic@gmail.com>
2023-10-19 17:35:49 -0400

    files to run analysis over N* and f1(1420) MC

---
+++ scripts/file_utils/run_f1_1420_analysis.py
@@ -0,0 +1,12 @@
+from my_library.flat_analysis import run_analysis
+
+# files = [
+#     '/work/halld/home/viducic/data/pimkpks/mc/f1_1420/f1_1420_kstar_minus_flat_bestX2.root',
+#     '/work/halld/home/viducic/data/pimkpks/mc/f1_1420/f1_1420_kstar_zero_flat_bestX2.root',
+#     '/work/halld/home/viducic/data/pipkmks/mc/f1_1420/f1_1420_kstar_plus_flat_bestX2.root',
+#     '/work/halld/home/viducic/data/pipkmks/mc/f1_1420/f1_1420_kstar_zero_flat_bestX2.root'
+# ]
+
+file_path = '/work/halld/home/viducic/data/{}/mc/f1_1420/f1_1420_kstar_{}_flat_bestX2.root'
+# TODO: updated run_analysis method to handle n* and f1_1420
+# TODO: call run_analysis for each file 
---
+++ scripts/file_utils/run_nstar_analysis.py
@@ -0,0 +1,5 @@
+from my_library.flat_analysis import run_analysis
+
+'/work/halld/home/viducic/data/pimkpks/mc/nstar'
+
+# TODO: write in same was as f1_1420 file 
