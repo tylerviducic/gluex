5628093b805914e92c5dd438a9288780197627ee
Tyler <tyler.viducic@gmail.com>
2023-10-24 12:39:33 -0400

    typo in 1420

--- scripts/file_utils/run_f1_1420_analysis.py
+++ scripts/file_utils/run_f1_1420_analysis.py
@@ -1,14 +1,5 @@
 from my_library.flat_analysis import run_analysis
 
-# files = [
-#     '/work/halld/home/viducic/data/pimkpks/mc/f1_1420/f1_1420_kstar_minus_flat_bestX2.root',
-#     '/work/halld/home/viducic/data/pimkpks/mc/f1_1420/f1_1420_kstar_zero_flat_bestX2.root',
-#     '/work/halld/home/viducic/data/pipkmks/mc/f1_1420/f1_1420_kstar_plus_flat_bestX2.root',
-#     '/work/halld/home/viducic/data/pipkmks/mc/f1_1420/f1_1420_kstar_zero_flat_bestX2.root'
-# ]
-
-file_path = '/work/halld/home/viducic/data/{}/mc/f1_1420/f1_1420_kstar_{}_flat_bestX2.root'
-
 charge_dict = {
     'pipkmks': ('plus', 'zero'),
     'pimkpks': ('minus', 'zero')
