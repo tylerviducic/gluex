be74c016dba79c79dd167df4630eea556e73d780
Tyler <tyler.viducic@gmail.com>
2023-11-02 11:44:41 -0400

    removed comments

--- my_library/flat_analysis.py
+++ my_library/flat_analysis.py
@@ -39,12 +39,11 @@ def run_analysis(channel, run_period, data_type, thrown=False, nstar_mass=None, 
         print('cuts done in {} seconds'.format(time.time() - start_time))
 
         ## SAVE FILTERED DATA FOR USE ELSEWHERE IF NEEDED ##
-        ## COMMENT/UNCOMMENT AS NEEDED WHEN CHANGING THINGS ABOVE THIS LINE ##
         output_filename = ct.get_filtered_file_output_name(channel, run_period, data_type, nstar_mass=nstar_mass, kstar_charge=kstar_charge)
         output_treename = ct.get_filtered_tree_output_name(channel, data_type)
         df.Snapshot(output_treename, f'{output_path}/{output_filename}', fLazy=True)
 
-        # ## FILTER BEAM AND T RANGE TO FIT WITHIN THE INDEX SET EARLIER ##
+        ## FILTER BEAM AND T RANGE TO FIT WITHIN THE INDEX SET EARLIER ##
         df = df.Filter(kcuts.BEAM_RANGE).Filter(kcuts.T_RANGE)
 
         if channel == 'pipkmks':
