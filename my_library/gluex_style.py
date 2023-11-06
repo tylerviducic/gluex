9f6d2eb5d2e35724ead76be618d577bf112a3ac8
Tyler <tyler.viducic@gmail.com>
2023-11-05 21:09:04 -0500

    updated style guide

--- my_library/gluex_style.py
+++ my_library/gluex_style.py
@@ -1,7 +1,6 @@
 import ROOT
 
 
-# TODO: font size 
 # define a default style for GlueX plots
 gluex_style = ROOT.TStyle("gluex", "Default GlueX Style")
 
@@ -9,7 +8,7 @@ gluex_style.SetCanvasBorderMode(0)
 gluex_style.SetPadBorderMode(0)
 gluex_style.SetPadColor(0)
 gluex_style.SetCanvasColor(0)
-gluex_style.SetTitleColor(0)
+gluex_style.SetTitleColor(ROOT.kBlack, "xyz")
 gluex_style.SetStatColor(0)
 
 # default window sizes
@@ -17,18 +16,19 @@ gluex_style.SetCanvasDefW(800)
 gluex_style.SetCanvasDefH(600)
 
 # default margins 
-gluex_style.SetPadBottomMargin(0.15)
-gluex_style.SetPadLeftMargin(0.15)
+gluex_style.SetPadBottomMargin(0.11)
+gluex_style.SetPadLeftMargin(0.11)
 gluex_style.SetPadTopMargin(0.05)
-gluex_style.SetPadRightMargin(0.08)
+gluex_style.SetPadRightMargin(0.05)
 
 # axis labels and settings
 gluex_style.SetStripDecimals(0);
-gluex_style.SetLabelSize(0.055,"xyz") # size of axis value font
-gluex_style.SetTitleSize(0.06,"xyz") # size of axis title font
+gluex_style.SetLabelSize(0.035,"xyz") # size of axis value font
+gluex_style.SetTitleSize(0.04,"xyz") # size of axis title font
 gluex_style.SetTitleFont(42,"xyz") # font option
 gluex_style.SetLabelFont(42,"xyz")  
-gluex_style.SetTitleOffset(1.2,"y")  
+gluex_style.SetTitleOffset(1.25,"y")  
+gluex_style.SetTitleOffset(1.1,"x")  
 gluex_style.SetLabelOffset(0.01,"xyz") # stop collisions of "0"s at the origin
 
 # histogram settings
@@ -50,7 +50,7 @@ gluex_style.SetHistFillColor(920) # grey
 # pick a palette for 2D plots
 # gluex_style.SetPalette(ROOT.kViridis) 
 # #gluex_style.SetPalette(ROOT.kBird) # default
-gluex_style.SetPalette(ROOT.kCividis) # default
+gluex_style.SetPalette(ROOT.kCividis)
 # gluex_style.SetPalette(ROOT.kColorPrintableOnGrey) 
 # gluex_style.SetPalette(ROOT.kDarkBodyRadiator) # I like this one better...
 
