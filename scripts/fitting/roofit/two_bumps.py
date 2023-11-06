8bde0c1047f0b2dc0ede738dd567c604ce48080f
Tyler <tyler.viducic@gmail.com>
2023-11-06 17:53:41 -0500

    fits w/ 2 bumps, whatever.

--- scripts/fitting/roofit/two_bumps.py
+++ scripts/fitting/roofit/two_bumps.py
@@ -7,22 +7,32 @@ import ROOT
 import my_library.common_analysis_tools as ct
 import my_library.constants as constants
 import my_library.kinematic_cuts as kcuts 
+import my_library.gluex_style as gxstyle
 import os
 
 os.nice(18)
 ROOT.EnableImplicitMT(8)
 ROOT.gStyle.SetOptStat(0)
 
-channel = 'pipkmks'
-# channel = 'pimkpks'
+# channel = 'pipkmks'
+channel = 'pimkpks'
 cut = 'all'
 
 if channel == 'pipkmks' :
     voight_resoltion = constants.F1_PIPKMKS_VOIGHT_SIGMA
     voight_resolution_error = constants.F1_PIPKMKS_VOIGHT_SIGMA_ERROR
+    # data_color = ROOT.TColor.GetColor(600) # kBlue
+    total_fit_color = 860
+    f1_color = 860-6
+    gaus_color = 880
+    background_color = 616
 elif channel == 'pimkpks' :
     voight_resoltion = constants.F1_PIMKPKS_VOIGHT_SIGMA
     voight_resolution_error = constants.F1_PIMKPKS_VOIGHT_SIGMA_ERROR
+    total_fit_color = 633
+    f1_color = 634
+    gaus_color = 887
+    background_color = 618
 
 data_hist = ct.get_integrated_gluex1_kstar_corrected_data_hist(channel)
 
@@ -39,8 +49,8 @@ voight_sigma.setConstant(True)
 # voight_m.setConstant(True)
 # voight_width.setConstant(True)
 
-gaus_m = ROOT.RooRealVar("gaus_m", "gaus_m", 1.37, 1.35, 1.41)
-gaus_width = ROOT.RooRealVar("gaus_width", "gaus_width", 0.03, 0.005, 0.05)
+gaus_m = ROOT.RooRealVar("gaus_m", "gaus_m", 1.37, 1.35, 1.43)
+gaus_width = ROOT.RooRealVar("gaus_width", "gaus_width", 0.034, 0.025, 0.05)
 gaus = ROOT.RooGaussian("gaus", "gaus", m_kkpi, gaus_m, gaus_width)
 # gaus_m.setConstant(True)
 # gaus_width.setConstant(True)
@@ -82,15 +92,38 @@ frame.GetXaxis().SetTitle(f'{title.split(" ")[0]} GeV')
 frame.GetYaxis().SetTitle(f'Counts/10MeV')
 
 dh.plotOn(frame, ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2))
-combined_pdf.plotOn(frame, ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['red'])))
+# dh.plotOn(frame, DataError="SumW2", LineColor="b", MarkerColor="b")
+combined_pdf.plotOn(frame, ROOT.RooFit.LineColor(total_fit_color), ROOT.RooFit.Name("full_fit"))
 pullHist = frame.pullHist()
-combined_pdf.plotOn(frame, ROOT.RooFit.Components("bkg"), ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['purple'])), ROOT.RooFit.LineStyle(ROOT.kDashed))
-combined_pdf.plotOn(frame, ROOT.RooFit.Components("voight"), ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['blue'])))
-combined_pdf.plotOn(frame, ROOT.RooFit.Components("gaus"), ROOT.RooFit.LineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['purple'])))
+combined_pdf.plotOn(frame, ROOT.RooFit.Components("voight"), ROOT.RooFit.LineColor(f1_color), ROOT.RooFit.DrawOption("F"), ROOT.RooFit.FillColor(f1_color), ROOT.RooFit.LineWidth(5), ROOT.RooFit.Name("f1_signal"))
+combined_pdf.plotOn(frame, ROOT.RooFit.Components("gaus"), ROOT.RooFit.LineColor(gaus_color), ROOT.RooFit.LineWidth(4), ROOT.RooFit.Name("1420_tail"))
+combined_pdf.plotOn(frame, ROOT.RooFit.Components("bkg"), ROOT.RooFit.LineColor(background_color), ROOT.RooFit.MarkerColor(background_color), ROOT.RooFit.LineWidth(4), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.Name("bkg"))
 
 frame.Draw()
+
+legend = ROOT.TLegend(0.6, 0.2, 0.9, 0.5)
+legend.AddEntry(frame.findObject("full_fit"), "Total Fit", "lpf")
+legend.AddEntry(frame.findObject("f1_signal"), "Signal", "lpf")
+legend.AddEntry(frame.findObject("1420_tail"), "f_{1}(1420) Tail", "lpf")
+legend.AddEntry(frame.findObject("bkg"), "Linear Background", "l")
+legend.Draw()
+
+# fit_params = ROOT.TPaveText(0.6, 0.5, 0.9, 0.8, "NDC")
+# fit_params.AddText(f"Mean = {voight_m.getVal() * 1000} +/- {voight_m.getError() * 1000}")
+# fit_params.AddText(f"Width = {voight_width.getVal() * 1000} +/- {voight_width.getError() * 1000}")
+# fit_params.AddText(f"#Chi^{2}/n.d.f = " + str(round(chi2_per_ndf, 2)))
+# fit_params.Draw()
+
+fit_params = ROOT.TLatex()
+fit_params.SetTextSize(0.045)
+fit_params.DrawLatexNDC(0.425, 0.875, f"Mean = {voight_m.getVal() * 1000:.2f} #pm {voight_m.getError() * 1000:.2f} MeV")
+fit_params.DrawLatexNDC(0.425, 0.825, f"Width = {voight_width.getVal() * 1000:.2f} #pm {voight_width.getError() * 1000:.2f} MeV")
+fit_params.DrawLatexNDC(0.425, 0.775, "#chi^{2}/ndf = " + '{:.2f}'.format(chi2_per_ndf))
+
+
 c1.Update()
 c1.SaveAs(f'/work/halld/home/viducic/plots/thesis/{channel}_integrated_fit_two_bumps.png')
+c1.SaveAs(f'/work/halld/home/viducic/scripts/plotting/dnp_maui/plots/{channel}_integrated_fit_two_bumps.png')
 
 pullDist = ROOT.TH1I("pullDist", "pullDist", 5, 0, 5)
 for i in range(0, pullHist.GetN()):
@@ -109,25 +142,25 @@ K-S test = 1 means very high probability of data coming from the
 distribution described by the model
 """
 
-c3 = ROOT.TCanvas("c2", "c2", 800, 600)
-c3.cd()
-c3.Divide(3, 1)
-c3.cd(1)
-pullHist.Draw("AP")
+# c3 = ROOT.TCanvas("c2", "c2", 800, 600)
+# c3.cd()
+# c3.Divide(3, 1)
+# c3.cd(1)
+# pullHist.Draw("AP")
 
-y = 0.0
+# y = 0.0
 
-line= ROOT.TLine(frame.GetXaxis().GetXmin(), y, frame.GetXaxis().GetXmax(), y)
-line.SetLineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['red']))
-line.SetLineStyle(2)
-line.SetLineWidth(2)
-line.Draw("same")
-c3.cd(2)
-pullDist.Draw()
-c3.cd(3)
-ks_test_data.Draw()
-ks_test_func.Draw("same")
-c3.Update()
+# line= ROOT.TLine(frame.GetXaxis().GetXmin(), y, frame.GetXaxis().GetXmax(), y)
+# line.SetLineColor(ROOT.TColor.GetColor(constants.COLORBLIND_HEX_DICT['red']))
+# line.SetLineStyle(2)
+# line.SetLineWidth(2)
+# line.Draw("same")
+# c3.cd(2)
+# pullDist.Draw()
+# c3.cd(3)
+# ks_test_data.Draw()
+# ks_test_func.Draw("same")
+# c3.Update()
 
 print(f"f1 mass = {voight_m.getVal() * 1000} +/- {voight_m.getError() * 1000}")
 print(f"f1 width = {voight_width.getVal() * 1000} +/- {voight_width.getError() * 1000}")
