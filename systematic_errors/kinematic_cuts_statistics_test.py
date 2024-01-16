387ff96c2bed98da67145b6345a1b5a3f33988bc
Tyler <tyler.viducic@gmail.com>
2024-01-16 14:12:34 -0500

    added charge conjugated channel

--- systematic_errors/kinematic_cuts_statistics_test.py
+++ systematic_errors/kinematic_cuts_statistics_test.py
@@ -41,21 +41,58 @@ nominal_cuts_dict_pipkmks = {
             'mx2_all': cuts.MX2_PPIPKMKS_CUT
             }
 
+varied_cuts_dict_pimkpks = {
+            'kinfit_cl': ('kinfit_cl > 1e-6', 'kinfit_cl > 1e-4'),
+            'pathlength': ('pathlength_sig > 4', 'pathlength_sig > 6'),
+            'ks_m': (f'abs(ks_m - {constants.KSHORT_FIT_MEAN}) < 2.5 * {constants.KSHORT_FIT_WIDTH}', f'abs(ks_m - {constants.KSHORT_FIT_MEAN}) < 1.5 * {constants.KSHORT_FIT_WIDTH}'),
+            'ppi': ('ppim_m > 1.3', 'ppim_m > 1.5'),
+            'kp': ('kpp_m > 1.9', 'kpp_m > 2.1'),
+            'ksp': ('ksp_m > 1.9', 'ksp_m > 2.1'),
+            'pp': ('p_p > 0.35', 'p_p > 0.45'),
+            'neutral_kstar': ('abs(kppim_m - 0.89555) > (1.5 * 0.0473)', 'abs(kppim_m - 0.89555) > (2.5 * 0.0473)'),
+            'charged_kstar': ('abs(kspim_m - 0.89167) > (1.75 * 0.0514)', 'abs(kspim_m - 0.89167) > (2.25 * 0.0514)'),
+            'mx2_all': ('abs(mx2_ppimkpks) < 0.02', 'abs(mx2_ppimkpks) < 0.007')
+             }
+
+nominal_cuts_dict_pimkpks = {
+            'kinfit_cl': cuts.KINFIT_CL_CUT,
+            'pathlength': cuts.KS_PATHLENGTH_CUT,
+            'ks_m': cuts.KS_MASS_CUT,
+            'ppi': cuts.PPIM_MASS_CUT,
+            'kp': cuts.KPP_MASS_CUT,
+            'ksp': cuts.KSP_MASS_CUT,
+            'pp': cuts.P_P_CUT,
+            'neutral_kstar': cuts.KSTAR_ZERO_CUT_PIMKPKS,
+            'charged_kstar': cuts.KSTAR_MINUS_CUT,
+            'mx2_all': cuts.MX2_PPIMKPKS_CUT
+            }
+
 df_pipkmks = tools.get_dataframe('pipkmks', 'gluex1', 'data', filtered=False)
+df_pimkpks = tools.get_dataframe('pimkpks', 'gluex1', 'data', filtered=False)
 
 variation_stats_dict_pipkmks = {}
+variation_stats_dict_pimkpks = {}
 
 for varied_cut in varied_cuts_dict_pipkmks:
     # print(varied_cut)
-    varied_df = df_pipkmks.Filter("true")
+    varied_df_pipkmks = df_pipkmks.Filter("true")
+    varied_df_pimkpks = df_pimkpks.Filter("true")
     for nominal_cut in nominal_cuts_dict_pipkmks:
         if varied_cut == nominal_cut:
             continue
         else:
-            varied_df = varied_df.Filter(nominal_cuts_dict_pipkmks[nominal_cut])
-    df_loose = varied_df.Filter(varied_cuts_dict_pipkmks[varied_cut][0])
-    df_tight = varied_df.Filter(varied_cuts_dict_pipkmks[varied_cut][1])
-    variation_stats_dict_pipkmks[varied_cut] = (df_loose.Filter(cuts.F1_SIGNAL_REGION_PIPKMKS).Count(), df_tight.Filter(cuts.F1_SIGNAL_REGION_PIPKMKS).Count())
+            varied_df_pipkmks = varied_df_pipkmks.Filter(nominal_cuts_dict_pipkmks[nominal_cut])
+            varied_df_pimkpks = varied_df_pimkpks.Filter(nominal_cuts_dict_pimkpks[nominal_cut])
+    df_loose_pipkmks = varied_df_pipkmks.Filter(varied_cuts_dict_pipkmks[varied_cut][0])
+    df_loose_pimkpks = varied_df_pimkpks.Filter(varied_cuts_dict_pimkpks[varied_cut][0])
+    df_tight_pipkmks = varied_df_pipkmks.Filter(varied_cuts_dict_pipkmks[varied_cut][1])
+    df_tight_pimkpks = varied_df_pimkpks.Filter(varied_cuts_dict_pimkpks[varied_cut][1])
+    variation_stats_dict_pipkmks[varied_cut] = (df_loose_pipkmks.Filter(cuts.F1_SIGNAL_REGION_PIPKMKS).Count(), df_tight_pipkmks.Filter(cuts.F1_SIGNAL_REGION_PIPKMKS).Count())
+    variation_stats_dict_pimkpks[varied_cut] = (df_loose_pimkpks.Filter(cuts.F1_SIGNAL_REGION_PIMKPKS).Count(), df_tight_pimkpks.Filter(cuts.F1_SIGNAL_REGION_PIMKPKS).Count())
     
+print('\nPi+K-Ks')
 for variation in variation_stats_dict_pipkmks:
     print(f'{variation}: loose -- {abs((variation_stats_dict_pipkmks[variation][0].GetValue()-baseline_pipkmks))/baseline_pipkmks * 100} || tight -- {abs((variation_stats_dict_pipkmks[variation][1].GetValue()-baseline_pipkmks))/baseline_pipkmks * 100}')
+print('\nPi-K+Ks')
+for variation in variation_stats_dict_pimkpks:
+    print(f'{variation}: loose -- {abs((variation_stats_dict_pimkpks[variation][0].GetValue()-baseline_pimkpks))/baseline_pimkpks * 100} || tight -- {abs((variation_stats_dict_pimkpks[variation][1].GetValue()-baseline_pimkpks))/baseline_pimkpks * 100}')
