c07246f9f3aa21cb82078d001f9c90cc5bd587e2
Tyler <tyler.viducic@gmail.com>
2024-02-05 13:05:21 -0500

    Add code to correct data histograms for varied kstar efficiency and write function to fit histograms

--- systematic_errors/fit_histograms.py
+++ systematic_errors/fit_histograms.py
@@ -5,10 +5,12 @@ import my_library.gluex_style as gluex_style
 import my_library.kinematic_cuts as cuts
 import os
 from my_library.systematics_constants import VARIED_CUTS_DICT_PIPKMKS, NOMINAL_CUTS_DICT_PIPKMKS, VARIED_CUTS_DICT_PIMKPKS, NOMINAL_CUTS_DICT_PIMKPKS
+import pandas as pd
 
 ROOT.EnableImplicitMT(12)
 
 # TODO: write fitting pipeline. calculate cross section for each energy and t bin
+# FINISH CODE AT LINE 104
 
 def get_binned_data_hist(channel, cut, e, t_bin_index, ltn):
     if ltn not in ['loose', 'tight', 'nominal']:
@@ -78,6 +80,31 @@ def get_gluex1_acceptance(channel, cut, e, t_bin_index, ltn):
     return acceptance
 
 
+def correct_data_hist_for_varied_kstar_efficiency(hist, cut, lt):
+    if lt not in ['loose', 'tight']:
+        raise ValueError(f'Invalid ltn value: {lt}. Must be one of "loose", "tight"')
+    
+    new_hist = hist.Clone()
+    new_hist.Sumw2()
+    kstar_efficiency_df = pd.read_csv(
+        f'/work/halld/home/viducic/systematic_errors/kstar_eff/kstar_cut_efficiency_{cut}_{lt}_10.0.csv')
+    for i in range(1, hist.GetXaxis().GetNbins()+1):
+        bin_ef_df = kstar_efficiency_df.loc[kstar_efficiency_df.mass_bin_center == round(
+            hist.GetXaxis().GetBinCenter(i), 3)]
+        if len(bin_ef_df) == 0:
+            print(
+                f'Bin center = {hist.GetXaxis().GetBinCenter(i)} has no efficiency value')
+            continue
+        bin_eff = bin_ef_df.kstar_cut_efficiency.values[0]
+        hist.SetBinContent(i, hist.GetBinContent(i) / bin_eff)
+        hist.SetBinError(i, hist.GetBinError(i) / bin_eff)
+    hist.SetDirectory(0)
+    return hist
+
+# TODO: write function to fit histograms
+def fit_hist(hist, param_guesses):
+    return
+
 
 if __name__ == '__main__':
     print('Running')
@@ -89,5 +116,15 @@ if __name__ == '__main__':
                     nominal_data_hist = get_binned_data_hist(channel, cut, e, t, 'nominal')
                     loose_data_hist = get_binned_data_hist(channel, cut, e, t, 'loose')
                     tight_data_hist = get_binned_data_hist(channel, cut, e, t, 'tight')
+
+                    nominal_cor_hist = tools.correct_data_hist_for_kstar_efficiency(nominal_data_hist)
+                    if cut not in ['neutral_kstar', 'charged_kstar']:
+                        eff_cor_hist_loose = correct_data_hist_for_varied_kstar_efficiency(loose_data_hist, cut, 'loose')
+                        eff_cor_hist_tight = correct_data_hist_for_varied_kstar_efficiency(tight_data_hist, cut, 'tight')
+                    else: 
+                        eff_cor_hist_loose = tools.correct_data_hist_for_kstar_efficiency(loose_data_hist)
+                        eff_cor_hist_tight = tools.correct_data_hist_for_kstar_efficiency(tight_data_hist)
+
+                    
 
 
