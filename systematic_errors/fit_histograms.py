ceafb3f49b15fba883dec36b115536446227019c
Tyler <tyler.viducic@gmail.com>
2024-02-06 16:04:10 -0500

    Add fitting functions and update parameter guesses

--- systematic_errors/fit_histograms.py
+++ systematic_errors/fit_histograms.py
@@ -1,3 +1,5 @@
+from typing import Dict
+from unittest import result
 import ROOT
 import my_library.common_analysis_tools as tools
 import my_library.constants as constants
@@ -10,7 +12,6 @@ import pandas as pd
 ROOT.EnableImplicitMT(12)
 
 # TODO: write fitting pipeline. calculate cross section for each energy and t bin
-# FINISH CODE AT LINE 104
 
 def get_binned_data_hist(channel, cut, e, t_bin_index, ltn):
     if ltn not in ['loose', 'tight', 'nominal']:
@@ -101,17 +102,137 @@ def correct_data_hist_for_varied_kstar_efficiency(hist, cut, lt):
     hist.SetDirectory(0)
     return hist
 
+
+def check_param_guess_structure(param_guesses: dict):
+    param_keys = ['voigt_amplitude', 'voigt_mean', 'voigt_sigma', 'voigt_width', 'gaus_amplitude', 'gaus_mean', 'gaus_width', 'bkg_par1', 'bkg_par2', 'bkg_par3']
+    if len(param_guesses) != 10:
+        raise ValueError('Invalid number of parameter guesses. Must be 10')
+    for guess_key, correct_key in zip(param_guesses.keys(), param_keys):
+        if guess_key != correct_key:
+            raise ValueError(f'Invalid parameter guess key: {guess_key}. was expecting {correct_key}')
+    return True
+
+
 # TODO: write function to fit histograms
-def fit_hist(hist, param_guesses):
+def fit_hist(hist, param_guesses: dict, cut, e, t, ltn):
+    if ltn not in ['loose', 'tight', 'nominal']:
+        raise ValueError(f'Invalid ltn value: {ltn}. Must be one of "loose", "tight", "nominal"')
+    
+    check_param_guess_structure(param_guesses)
+    
+    func = ROOT.TF1(f'func_{cut}_{e}_{t}_{ltn}', '[0]*TMath::Voigt(x-[1], [2], [3]) + gaus(4) + pol2(7)', 1.15, 1.51)
+
+    for par_name in param_guesses.keys():
+        func.SetParameter(par_name, param_guesses[par_name])
+
+    func.SetParameter('voigt_amplitude', param_guesses['voigt_amplitude']) # voigt amplitude
+    func.SetParLimits('voigt_amplitude', 0.1, 100000)
+    func.FixParameter('voigt_mean', param_guesses['voigt_mean']) # voigt mean
+    func.FixParameter('voigt_sigma', param_guesses['voigt_sigma']) # voigt sigma/resolution 
+    func.FixParameter('voigt_width', param_guesses['voigt_width']) # voigt width
+    func.SetParameter('voigt_amplitude', param_guesses['gaus_amplitude']) # gaus amplitude
+    func.SetParLimits('voigt_amplitude', 0.1, 10000)
+    func.FixParameter('gaus_mean', param_guesses['gaus_mean'])
+    func.FixParameter('gaus_width', param_guesses['gaus_width']) # gaus width
+    func.SetParameter('bkg_par1', param_guesses['bkg_par1']) # bkg par1
+    func.SetParLimits('bkg_par1', -100000, 0.0)
+    func.SetParameter('bkg_par2', param_guesses['bkg_par2']) # bkg par2
+    func.SetParameter('bkg_par3', param_guesses['bkg_par3']) # bkg par3
+
+    result = hist.Fit(func, 'SRBE0')
+    func.SetDirectory(0)
+    return result, func
+
+
+def update_guesses(func):
+    new_guesses = {'voigt_amplitude': func.GetParameter(0), 
+               'voigt_mean': func.GetParameter(1), 
+               'voigt_sigma': func.GetParameter(2),
+               'voigt_width': func.GetParameter(3), 
+               'gaus_amplitude': func.GetParameter(4), 
+               'gaus_mean': func.GetParameter(5), 
+               'gaus_width': func.GetParameter(6), 
+               'bkg_par1': func.GetParameter(7), 
+               'bkg_par2': func.GetParameter(8), 
+               'bkg_par3': func.GetParameter(9)}
+    return new_guesses
+
+
+def get_func_components(func, e, t, cut, ltn):
+    voigt = ROOT.TF1(f'voigt_{cut}_{e}_{t}', '[0]*TMath::Voigt(x-[1], [2], [3])', 1.15, 1.51)
+    gaus = ROOT.TF1(f'gaus_{cut}_{e}_{t}', 'gaus(0)', 1.15, 1.51)
+    bkg = ROOT.TF1(f'bkg_{cut}_{e}_{t}', 'pol2(0)', 1.15, 1.51)
+
+    voigt.SetParameter(0, func.GetParameter(0))
+    voigt.SetParameter(1, func.GetParameter(1))
+    voigt.SetParameter(2, func.GetParameter(2))
+    voigt.SetParameter(3, func.GetParameter(3))
+
+    gaus.SetParameter(0, func.GetParameter(4))
+    gaus.SetParameter(1, func.GetParameter(5))
+    gaus.SetParameter(2, func.GetParameter(6))
+
+    bkg.SetParameter(0, func.GetParameter(7))
+    bkg.SetParameter(1, func.GetParameter(8))
+    bkg.SetParameter(2, func.GetParameter(9))
+
+    voigt.SetDirectory(0)
+    gaus.SetDirectory(0)
+    bkg.SetDirectory(0)
+
+    return voigt, gaus, bkg
+
+# TODO: write function to store fit results in CSV file
+def store_dataframe_info(func_total, func_voigt, luminosity, e, t, cut, ltn):
     return
 
-
 if __name__ == '__main__':
+
     print('Running')
     channels = ['pipkmks', 'pimkpks']
+
+    c = ROOT.TCanvas('c', 'c', 1000, 1000)
+
     for channel in channels:
+
+        if channel == 'pipkmks' :
+            v_mean = constants.F1_PIPKMKS_voigt_MEAN
+            v_width = constants.F1_PIPKMKS_voigt_WIDTH
+            total_fit_color = ROOT.kViolet
+            f1_color = ROOT.kBlue
+            # background_color = ROOT.kOrange
+            background_color = total_fit_color
+            hist_title = 'K^{-}K_{s}#pi^{+}'
+            gaus_mean = constants.F1_PIPKMKS_GAUS_MEAN
+            gaus_width = constants.F1_PIPKMKS_GAUS_WIDTH
+        elif channel == 'pimkpks' :
+            v_mean = constants.F1_PIMKPKS_voigt_MEAN
+            v_width = constants.F1_PIMKPKS_voigt_WIDTH
+            total_fit_color = ROOT.kViolet +9
+            f1_color = ROOT.kRed
+            # background_color = ROOT.kTeal-5
+            background_color = total_fit_color
+            hist_title = 'K^{+}K_{s}#pi^{-}'
+            gaus_mean = constants.F1_PIMKPKS_GAUS_MEAN
+            gaus_width = constants.F1_PIMKPKS_GAUS_WIDTH
+
         for cut in VARIED_CUTS_DICT_PIPKMKS:
             for e in range(8, 12):
+
+                param_guesses = {
+                    'voigt_amplitude': 5, # voigt amplitude
+                    'voigt_mean': v_mean, # voigt mean
+                    'voigt_width': v_width, # voigt width
+                    'gaus_amplitude': 15, # gaus amplitude
+                    'gaus_mean': gaus_mean, # gaus mean
+                    'gaus_width': gaus_width, # gaus width
+                    'bkg_par1': -100, # bkg par1
+                    'bkg_par2': 100, # bkg par2
+                    'bkg_par3': 1 # bkg par3
+                }
+
+                e_flux = tools.get_luminosity_gluex_1(e-0.5, e+0.5)*1000
+
                 for t in range(1, 8):
                     nominal_data_hist = get_binned_data_hist(channel, cut, e, t, 'nominal')
                     loose_data_hist = get_binned_data_hist(channel, cut, e, t, 'loose')
@@ -124,6 +245,76 @@ if __name__ == '__main__':
                     else: 
                         eff_cor_hist_loose = tools.correct_data_hist_for_kstar_efficiency(loose_data_hist)
                         eff_cor_hist_tight = tools.correct_data_hist_for_kstar_efficiency(tight_data_hist)
+
+                    result_nominal, func_nominal = fit_hist(nominal_cor_hist, param_guesses, cut, e, t, 'nominal')
+                    result_loose, func_loose = fit_hist(eff_cor_hist_loose, param_guesses, cut, e, t, 'loose')
+                    result_tight, func_tight = fit_hist(eff_cor_hist_tight, param_guesses, cut, e, t, 'tight')
+
+                    voigt_nominal, gaus_nominal, bkg_nominal = get_func_components(func_nominal, e, t, cut, 'nominal')
+                    voigt_loose, gaus_loose, bkg_loose = get_func_components(func_loose, e, t, cut, 'loose')
+                    voigt_tight, gaus_tight, bkg_tight = get_func_components(func_tight, e, t, cut, 'tight')
+
+                    func_nominal.SetLineColor(total_fit_color)
+                    func_loose.SetLineColor(total_fit_color)
+                    func_tight.SetLineColor(total_fit_color)
+
+                    voigt_nominal.SetLineColor(ROOT.kBlack)
+                    voigt_loose.SetLineColor(ROOT.kBlack)
+                    voigt_tight.SetLineColor(ROOT.kBlack)
+
+                    voigt_nominal.SetFillColor(f1_color)
+                    voigt_loose.SetFillColor(f1_color)
+                    voigt_tight.SetFillColor(f1_color)
+
+                    gaus_nominal.SetLineColor(background_color)
+                    gaus_loose.SetLineColor(background_color)
+                    gaus_tight.SetLineColor(background_color)
+
+                    gaus_nominal.SetLineStyle(3)
+                    gaus_loose.SetLineStyle(3)
+                    gaus_tight.SetLineStyle(3)
+
+                    bkg_nominal.SetLineColor(background_color)
+                    bkg_loose.SetLineColor(background_color)
+                    bkg_tight.SetLineColor(background_color)
+
+                    bkg_nominal.SetLineStyle(2)
+                    bkg_loose.SetLineStyle(2)
+                    bkg_tight.SetLineStyle(2)
+
+                    c.Clear()
+                    c.Divide(1, 3)
+                    c.cd(1)
+                    func_loose.Draw()
+                    voigt_loose.Draw('same')
+                    gaus_loose.Draw('same')
+                    bkg_loose.Draw('same')
+                    c.cd(2)
+                    func_nominal.Draw()
+                    voigt_nominal.Draw('same')
+                    gaus_nominal.Draw('same')
+                    bkg_nominal.Draw('same')
+                    c.cd(3)
+                    func_tight.Draw()
+                    voigt_tight.Draw('same')
+                    gaus_tight.Draw('same')
+                    bkg_tight.Draw('same')
+
+                    c.Update()
+                    c.SaveAs(f'/work/halld/home/viducic/systematic_errors/kstar_eff/plots/{channel}_{cut}_e{e}_t{t}_fit.png')
+
+
+
+
+
+                    
+
+
+
+                    
+
+
+
 
                     
 
