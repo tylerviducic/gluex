89161c9479502b2033bb675ec4f63b73afbc05c6
Tyler <tyler.viducic@gmail.com>
2024-03-22 14:14:19 -0400

    create dselectors to plot timing cuts and other various quantities like d/dx and whatnot

---
+++ scripts/plotting/DSelector_plot_delta_t.h
@@ -0,0 +1,76 @@
+#ifndef DSelector_plot_delta_t_h
+#define DSelector_plot_delta_t_h
+
+#include <iostream>
+
+#include "DSelector/DSelector.h"
+#include "DSelector/DHistogramActions.h"
+#include "DSelector/DCutActions.h"
+
+#include "TH1I.h"
+#include "TH2I.h"
+
+class DSelector_plot_delta_t : public DSelector
+{
+	public:
+
+		DSelector_plot_delta_t(TTree* locTree = NULL) : DSelector(locTree){}
+		virtual ~DSelector_plot_delta_t(){}
+
+		void Init(TTree *tree);
+		Bool_t Process(Long64_t entry);
+
+	private:
+
+		void Get_ComboWrappers(void);
+		void Finalize(void);
+
+		// BEAM POLARIZATION INFORMATION
+		UInt_t dPreviousRunNumber;
+		bool dIsPolarizedFlag; //else is AMO
+		bool dIsPARAFlag; //else is PERP or AMO
+
+		bool dIsMC;
+
+		// ANALYZE CUT ACTIONS
+		// // Automatically makes mass histograms where one cut is missing
+		DHistogramAction_AnalyzeCutActions* dAnalyzeCutActions;
+
+		//CREATE REACTION-SPECIFIC PARTICLE ARRAYS
+
+		//Step 0
+		DParticleComboStep* dStep0Wrapper;
+		DBeamParticle* dComboBeamWrapper;
+		DChargedTrackHypothesis* dPiPlus1Wrapper;
+		DChargedTrackHypothesis* dKMinusWrapper;
+		DChargedTrackHypothesis* dProtonWrapper;
+
+		//Step 1
+		DParticleComboStep* dStep1Wrapper;
+		DChargedTrackHypothesis* dPiMinusWrapper;
+		DChargedTrackHypothesis* dPiPlus2Wrapper;
+
+		// DEFINE YOUR HISTOGRAMS HERE
+		// EXAMPLES:
+		TH1I* dHist_MissingMassSquared;
+		TH1I* dHist_BeamEnergy;
+
+	ClassDef(DSelector_plot_delta_t, 0);
+};
+
+void DSelector_plot_delta_t::Get_ComboWrappers(void)
+{
+	//Step 0
+	dStep0Wrapper = dComboWrapper->Get_ParticleComboStep(0);
+	dComboBeamWrapper = static_cast<DBeamParticle*>(dStep0Wrapper->Get_InitialParticle());
+	dPiPlus1Wrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(0));
+	dKMinusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(1));
+	dProtonWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(3));
+
+	//Step 1
+	dStep1Wrapper = dComboWrapper->Get_ParticleComboStep(1);
+	dPiMinusWrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(0));
+	dPiPlus2Wrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(1));
+}
+
+#endif // DSelector_plot_delta_t_h
