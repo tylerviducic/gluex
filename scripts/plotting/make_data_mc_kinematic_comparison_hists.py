9338c215eeaeb9d08d59e534f56599ced5091e84
Tyler <tyler.viducic@gmail.com>
2024-04-29 12:00:27 -0400

    plot recon dists. make file comparison hists

---
+++ scripts/plotting/make_data_mc_kinematic_comparison_hists.py
@@ -0,0 +1,8 @@
+import ROOT
+import my_library.common_analysis_tools as tools
+import my_library.constants as constants
+import my_library.kinematic_cuts as kcuts
+import os
+
+ROOT.EnableImplicitMT()
+os.nice(18)
